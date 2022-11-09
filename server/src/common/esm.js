2e2b0ea92f180d7f294563b5826482550db2daba
Antoine Bigard <bigard.antoine@gmail.com>
2022-11-09 08:40:44 +0100

    chore: wip

--- server/src/common/esm.js
+++ server/src/common/esm.js
@@ -1,10 +1,12 @@
 import { createRequire } from "module";
-import { dirname } from "path";
+import path from "path";
 import { fileURLToPath } from "url";
 const require = createRequire(import.meta.url);
 
 export const packageJson = require("../../package.json");
 
-export function getDirname(base) {
-  return dirname(fileURLToPath(base));
-}
+// add import.meta.url to filePath
+export const __dirname = (filePath) => {
+  const __filename = fileURLToPath(filePath);
+  return path.dirname(__filename);
+};
