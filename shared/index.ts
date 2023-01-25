425d63b73e2d59ba79584565e80f508c791955c8
Florian DE LA COMBLE <f.delacomble@gmail.com>
2023-01-25 18:09:52 +0100

    Monorepo config (#4)
    * config monorepo
    
    * chore: wip monorepo
    
    * chore: wip monorepo
    
    * chore: simplification de l'exemple
    
    * chore: simplification du makefile
    
    * chore: simplification du makefile
    
    * chore: simplification du makefile
    
    * chore: simplification du makefile
    
    * chore: correction du lint
    
    * chore: correction du lint

---
+++ shared/index.ts
@@ -0,0 +1,3 @@
+"use client";
+
+export { SharedCompo } from "./SharedCompo";
--- ui/app/page.tsx
+++ ui/app/page.tsx
@@ -1,3 +1,11 @@
+import { useEffect } from "react";
+import { SharedCompo } from "shared";
+
 export default function Home() {
-  return <main>Hello world</main>;
+  return (
+    <main>
+      <div>Hello world</div>
+      <SharedCompo />
+    </main>
+  );
 }
