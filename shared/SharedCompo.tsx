a03fc2223a55d9324af8af5c4792910d3f264103
flodlc <f.delacomble@gmail.com>
2023-01-30 15:04:32 +0100

    chore: eslint global

--- shared/SharedCompo.tsx
+++ shared/SharedCompo.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useEffect } from "react";
 
 export const SharedCompo = () => {
--- ui/app/page.tsx
+++ ui/app/page.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { SharedCompo } from "shared";
 
 export default function Home() {
