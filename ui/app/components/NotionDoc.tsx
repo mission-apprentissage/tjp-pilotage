b7cfdac5dd7f5a81049e2f0de5b7beb2b7138c7a
flodlc <f.delacomble@gmail.com>
2023-03-23 11:27:24 +0100

    ajout de plausible

--- ui/app/components/NotionDoc.tsx
+++ ui/app/components/NotionDoc.tsx
@@ -1,8 +1,8 @@
 "use client";
+import { ExtendedRecordMap } from "notion-types";
 import { Suspense } from "react";
 import { NotionRenderer } from "react-notion-x";
-
-export const Doc = ({ recordMap }: { recordMap: any }) => {
+export const Doc = ({ recordMap }: { recordMap: ExtendedRecordMap }) => {
   return (
     <Suspense>
       <NotionRenderer
