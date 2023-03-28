ec24818c2d68beedeacc3ab8fc51f2cd5f854ba7
flodlc <f.delacomble@gmail.com>
2023-03-28 19:03:57 +0200

    ajout des redirections

--- ui/app/etablissementss/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <iframe
-      style={{ width: "100%", flex: 1 }}
-      src="https://pilotage-recette.trajectoirespro.beta.gouv.fr/metabase/public/dashboard/e917107f-0d3a-4846-8b45-6dc48ed81b6e"
-      frameBorder="0"
-    />
-  );
-}
