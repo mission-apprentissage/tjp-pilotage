2eca830020d13c57949a5b81f65f57c0f8a7eb31
flodlc <f.delacomble@gmail.com>
2023-05-02 15:39:04 +0200

    fix: sql migration

--- server/migrations/20230427152156_alter_table_indicateurEntree.ts
+++ server/migrations/20230427152156_alter_table_indicateurEntree.ts
@@ -2,11 +2,11 @@ import { db, pool } from "../src/db/zapatos";
 
 export async function up() {
   await db.sql`
+  ALTER TABLE "indicateurEntree" RENAME COLUMN "millesimeEntree" TO "rentreeScolaire";
   ALTER TABLE "indicateurEntree"
     ADD COLUMN "effectifs" jsonb,
     ADD COLUMN "capacites" jsonb,
-    ADD COLUMN "anneeDebut" int,
-    RENAME COLUMN "millesimeEntree" TO "rentreeScolaire"
+    ADD COLUMN "anneeDebut" int;
   `.run(pool);
 }
 
