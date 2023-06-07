9cf2b711c5a4b4990f2355074fffa40aac22916d
flodlc <f.delacomble@gmail.com>
2023-06-07 18:20:38 +0200

    feat: wip panorama

--- ui/app/panorama/[codeRegion]/InfoSection.tsx
+++ ui/app/panorama/[codeRegion]/InfoSection.tsx
@@ -12,6 +12,22 @@ import {
   Text,
 } from "@chakra-ui/react";
 
+const lienDares: Record<string, string> = {
+  84: "https://dares.travail-emploi.gouv.fr/publication/auvergne-rhone-alpes-quelles-difficultes-de-recrutement-dici-2030",
+  53: "https://dares.travail-emploi.gouv.fr/publication/bretagne-quelles-difficultes-de-recrutement-dici-2030",
+  27: "https://dares.travail-emploi.gouv.fr/publication/bourgogne-franche-comte-quelles-difficultes-de-recrutement-dici-2030",
+  24: "https://dares.travail-emploi.gouv.fr/publication/centre-val-de-loire-quelles-difficultes-de-recrutement-dici-2030",
+  94: "https://dares.travail-emploi.gouv.fr/publication/corse-quelles-difficultes-de-recrutement-dici-2030",
+  44: "https://dares.travail-emploi.gouv.fr/publication/grand-est-quelles-difficultes-de-recrutement-dici-2030",
+  32: "https://dares.travail-emploi.gouv.fr/publication/hauts-de-france-quelles-difficultes-de-recrutement-dici-2030",
+  11: "https://dares.travail-emploi.gouv.fr/publication/ile-de-france-quelles-difficultes-de-recrutement-dici-2030",
+  28: "https://dares.travail-emploi.gouv.fr/publication/normandie-quelles-difficultes-de-recrutement-dici-2030",
+  75: "https://dares.travail-emploi.gouv.fr/publication/nouvelle-aquitaine-quelles-difficultes-de-recrutement-dici-2030",
+  76: "https://dares.travail-emploi.gouv.fr/publication/occitanie-quelles-difficultes-de-recrutement-dici-2030",
+  52: "https://dares.travail-emploi.gouv.fr/publication/pays-de-la-loire-quelles-difficultes-de-recrutement-dici-2030",
+  93: "https://dares.travail-emploi.gouv.fr/publication/paca-quelles-difficultes-de-recrutement-dici-2030",
+};
+
 const InfoCard = ({
   title,
   description,
@@ -34,7 +50,13 @@ const InfoCard = ({
             <Text flex={1} mt={2}>
               {description}
             </Text>
-            <Button mt={4} variant="primary" as={Link} href={href}>
+            <Button
+              mt={4}
+              variant="primary"
+              as={Link}
+              href={href}
+              target="_blank"
+            >
               Accéder à l'information
             </Button>
           </Flex>
@@ -67,26 +89,26 @@ export const InfoSection = ({ codeRegion }: { codeRegion?: string }) => {
           <InfoCard
             title="Projection métier 2030"
             description="Retrouvez le dernier rapport de votre région"
-            href="google.fr"
-            img="phone_man.png"
+            href={(codeRegion && lienDares[codeRegion]) ?? ""}
+            img="/phone_man.png"
           />
           <InfoCard
             title="Métier en tension 2022"
             description="Retrouvez le dernier rapport de votre région"
-            href="google.fr"
-            img="looking_man.png"
+            href="https://dares.travail-emploi.gouv.fr/publication/les-tensions-sur-le-marche-du-travail-en-2021"
+            img="/looking_man.png"
           />
           <InfoCard
             title="Data emploi"
             description="Retrouvez les informations essentielles pour décrypter le marché du travail sur votre territoire"
             href={`https://dataemploi.pole-emploi.fr/panorama/REG/${codeRegion}`}
-            img="dashboard_girl.png"
+            img="/dashboard_girl.png"
           />
           <InfoCard
             title="Documentation région Voie professionelle"
             description="Retrouvez les informations essentielles pour décrypter les formations sur votre territoire"
-            href="google.fr"
-            img="team.png"
+            href=""
+            img="/team.png"
           />
         </SimpleGrid>
       </Container>
