17ffbc427731058b30d2412d63da72be1fbe1bd3
flodlc <f.delacomble@gmail.com>
2023-06-07 17:45:21 +0200

    feat: wip panorama

--- ui/app/panorama/page.tsx
+++ ui/app/panorama/page.tsx
@@ -1,6 +1,8 @@
 import { serverApi } from "@/api.client";
 import { PanoramaSelection } from "@/app/panorama/PanoramaSelection";
 
+export const revalidate = 60;
+
 export default async function Panorama() {
   const regionOptions = await serverApi.getRegions({}).call();
   return <PanoramaSelection regionOptions={regionOptions} />;
