131282cdb46c7f802f9bcea16ddfc6f69598d6cc
flodlc <f.delacomble@gmail.com>
2023-06-08 18:13:38 +0200

    fix: migration

--- server/migrations/20230517151418_alter_table_rawData.ts
+++ server/migrations/20230517151418_alter_table_rawData.ts
@@ -4,7 +4,8 @@ export async function up() {
   await db.sql`
   ALTER TABLE "rawData"
     ADD COLUMN "id" uuid PRIMARY KEY DEFAULT uuid_generate_v4();
-
+  CREATE EXTENSION pg_trgm;
+  CREATE EXTENSION btree_gin;
   CREATE INDEX "rawData_gin_data__type_id" 
       ON "rawData" USING GIN("type", data, id);
   `.run(pool);
