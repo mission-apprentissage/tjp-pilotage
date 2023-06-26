73202773c7002084d7d22c9ae90ff803991b87cf
flodlc <f.delacomble@gmail.com>
2023-06-26 11:43:28 +0200

    feat: overrides des cpc

--- server/src/modules/data/usecases/importFormationEtablissement/steps/importFormation/overrides.ts
+++ server/src/modules/data/usecases/importFormationEtablissement/steps/importFormation/overrides.ts
@@ -6,12 +6,20 @@ const overridesData = parse(overridesRef, {
   columns: true,
   skip_empty_lines: true,
   delimiter: ";",
-}) as { diplome: string; libelle: string; cfd?: string; rncp?: string }[];
+}) as {
+  diplome: string;
+  libelle: string;
+  "Code RNCP"?: string;
+  "Code diplôme"?: string;
+  "Commission professionnelle consultative"?: string;
+  Secteur?: string;
+  "Sous-secteur"?: string;
+}[];
 
 export const overrides = overridesData.reduce(
   (acc, cur) => ({
     ...acc,
-    [`${cur.diplome}_${cur.libelle}`]: { cfd: cur.cfd, rncp: cur.rncp },
+    [`${cur.diplome}_${cur.libelle}`]: cur,
   }),
-  {} as Record<string, { cfd?: string; rncp?: string }>
+  {} as Record<string, typeof overridesData[number]>
 );
