2d25d3fbf2972fe822520a7f6ba6dca47262322e
flodlc <f.delacomble@gmail.com>
2023-07-24 17:58:59 +0200

    fix docker-compose

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const path = require("path");
+// const path = require("path");
 
 const nextConfig = {
   transpilePackages: ['shared'],
   experimental: {
     appDir: true,
-    outputFileTracingRoot: path.join(__dirname, '../'),
+    // outputFileTracingRoot: path.join(__dirname, '../'),
   },
   // output: 'standalone'
 }
