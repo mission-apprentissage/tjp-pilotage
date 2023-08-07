8318266ffc82c736708fbb12a95505624f05f2b9
Lucas Détré <lucas.detre@gmail.com>
2023-08-07 18:03:26 +0200

    fix: ajout client pour l'entrée API

--- shared/client/client.ts
+++ shared/client/client.ts
@@ -2,8 +2,10 @@ import { AxiosInstance } from "axios";
 
 import { createEtablissementClient } from "./etablissements/etablissements.client";
 import { createFormationClient } from "./formations/formation.client";
+import { createPilotageReformeClient } from "./pilotageReforme/pilotageReforme.client";
 
 export const createClient = (instance: AxiosInstance) => ({
   ...createFormationClient(instance),
   ...createEtablissementClient(instance),
+  ...createPilotageReformeClient(instance),
 });
