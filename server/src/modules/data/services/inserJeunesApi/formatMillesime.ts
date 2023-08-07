46a67cf9535b3b51c00c0574a8e1fafabeb3d272
Lucas Détré <lucas.detre@gmail.com>
2023-08-07 16:49:14 +0200

    feat: (wip) requêtage des stats nationale/filtrées

---
+++ server/src/modules/data/services/inserJeunesApi/formatMillesime.ts
@@ -0,0 +1,28 @@
+// eslint-disable-next-line no-useless-escape
+export const checkMillesimeFormat = (millesime: string): boolean =>
+  /^\d{4}_\d{4}$/g.test(millesime);
+
+export const getMillesimeFromYear = (year: number): string => {
+  const currentMillesime = `${year - 1}_${year}`;
+
+  return currentMillesime;
+};
+
+export const getPreviousMillesime = (millesime: string): string => {
+  if (checkMillesimeFormat(millesime)) {
+    const startingYear = +millesime.substring(0, 4);
+    return `${startingYear - 1}_${startingYear}`;
+  }
+  return getCurrentMillesime();
+};
+
+export const getCurrentMillesime = (): string =>
+  getMillesimeFromYear(new Date().getFullYear());
+
+export const getNextMillesime = (millesime: string): string | null => {
+  if (checkMillesimeFormat(millesime)) {
+    const finishingYear = +millesime.substring(5);
+    return `${finishingYear}_${finishingYear + 1}`;
+  }
+  return null;
+};
