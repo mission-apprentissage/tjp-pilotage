d51e1bdc6d47c2cbec83ab65ef45e7b5d9b78648
flodlc <f.delacomble@gmail.com>
2023-08-11 12:22:44 +0200

    feat: token expiration

--- server/src/modules/auth/usecases/createUser/insertUserQuery.dep.ts
+++ server/src/modules/auth/usecases/createUser/insertUserQuery.dep.ts
@@ -4,6 +4,7 @@ export const insertUserQuery = async (user: {
   email: string;
   firstname?: string;
   lastname?: string;
+  role: string;
 }) => {
   await kdb.insertInto("user").values(user).execute();
 };
