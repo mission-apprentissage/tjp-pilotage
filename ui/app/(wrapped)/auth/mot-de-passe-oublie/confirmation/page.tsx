72b4d72c6df42d6b4b7dd793d6c4cc041da8a315
flodlc <f.delacomble@gmail.com>
2023-08-16 08:39:15 +0100

    typo

--- ui/app/(wrapped)/auth/mot-de-passe-oublie/confirmation/page.tsx
+++ ui/app/(wrapped)/auth/mot-de-passe-oublie/confirmation/page.tsx
@@ -7,7 +7,7 @@ export default () => {
     <Flex mt="12" mx="auto" direction="column" align="center">
       <Heading fontSize="lg">Demande confirmée !</Heading>
       <Text mt="2">
-        Un email de réinitialisation du mot de passe vous a été envoyée.
+        Un email de réinitialisation du mot de passe vous a été envoyé.
       </Text>
       <Button mt="6" as={NextLink} href="/" variant="primary">
         Retour à l'accueil
