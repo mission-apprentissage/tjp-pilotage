1a7e16e038375b7a9eb0d75663b7e88438622ebc
Lucas Détré <lucas.detre@gmail.com>
2023-08-17 18:45:50 +0200

    feat: wip: ajout UI pilotage de la réforme

--- shared/index.ts
+++ shared/index.ts
@@ -1,5 +1,6 @@
 export { FORMATIONS_COLUMNS } from "./FORMATIONS_COLUMNS";
 export { ETABLISSEMENTS_COLUMNS } from "./ETABLISSEMENTS_COLUMNS";
+export { PILOTAGE_REFORME_STATS_REGIONS_COLUMNS } from "./PILOTAGE_REFORME_STATS_REGIONS_COLUMNS";
 export { ROUTES_CONFIG } from "./client/ROUTES_CONFIG";
 export { createClient } from "./client/client";
 export type { ApiType } from "./client/clientFactory";
