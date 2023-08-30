19927cfb9ad80c59f1c8f1f8081893a020e457a9
Lucas Détré <lucas.detre@gmail.com>
2023-08-30 09:46:20 +0200

    feat: ajout du calcul du taux de devenir favorable aggrégé

--- server/src/modules/data/queries/utils/tauxDevenirFavorable.ts
+++ server/src/modules/data/queries/utils/tauxDevenirFavorable.ts
@@ -1,4 +1,6 @@
-import { sql } from "kysely";
+import { ExpressionBuilder, sql } from "kysely";
+
+import { DB } from "../../../../db/schema";
 
 const seuil = 20;
 
@@ -21,3 +23,53 @@ export const selectTauxDevenirFavorable = (
       + ${sql.table(indicateurSortieAlias)}."nbInsertion6mois")
     / ${selectDenominateurDevenirFavorable(indicateurSortieAlias)})
     end`;
+
+export const selectDenominateurDevenirFavorableAgg = (
+  indicateurSortieAlias: string
+) => sql<number>`
+    SUM(
+      case when ${sql.table(
+        indicateurSortieAlias
+      )}."nbPoursuiteEtudes" is not null
+      and ${sql.table(indicateurSortieAlias)}."nbInsertion6mois" is not null
+      then ${sql.table(indicateurSortieAlias)}."effectifSortie"
+      end
+    )`;
+
+export const selectTauxDevenirFavorableAgg = (
+  indicateurSortieAlias: string
+) => sql<number>`
+      case when
+      ${selectDenominateurDevenirFavorableAgg(
+        indicateurSortieAlias
+      )} >= ${seuil}
+      then (100 * (SUM(${sql.table(indicateurSortieAlias)}."nbPoursuiteEtudes")
+      + SUM(${sql.table(indicateurSortieAlias)}."nbInsertion6mois"))
+      / ${selectDenominateurDevenirFavorableAgg(indicateurSortieAlias)})
+      end
+    `;
+
+export function withDevenirFavorableReg({
+  eb,
+  millesimeSortie,
+  codeRegion,
+}: {
+  eb: ExpressionBuilder<DB, "formationEtablissement" | "etablissement">;
+  millesimeSortie: string;
+  codeRegion?: string | "ref";
+}) {
+  return eb
+    .selectFrom("indicateurRegionSortie as subIRS")
+    .whereRef("subIRS.cfd", "=", "formationEtablissement.cfd")
+    .whereRef("subIRS.dispositifId", "=", "formationEtablissement.dispositifId")
+    .where("subIRS.millesimeSortie", "=", millesimeSortie)
+    .$call((q) => {
+      if (!codeRegion) return q;
+      if (codeRegion === "ref") {
+        return q.whereRef("subIRS.codeRegion", "=", "etablissement.codeRegion");
+      }
+      return q.where("subIRS.codeRegion", "=", codeRegion);
+    })
+    .select([selectTauxDevenirFavorableAgg("subIRS").as("sa")])
+    .groupBy(["subIRS.cfd", "subIRS.dispositifId"]);
+}
