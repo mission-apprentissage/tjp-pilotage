e639c6542694b83ad5d4570324042009eed239ad
Florian De la comble <f.delacomble@gmail.com>
2023-08-30 14:04:57 +0200

    fix data files type location (#71)

--- server/src/modules/data/usecases/importFamillesMetiers/importFamillesMetiers.deps.ts
+++ server/src/modules/data/usecases/importFamillesMetiers/importFamillesMetiers.deps.ts
@@ -2,7 +2,7 @@ import { Insertable } from "kysely";
 
 import { kdb } from "../../../../db/db";
 import { DB } from "../../../../db/schema";
-import { NMefLine } from "../../files/NMef";
+import { NMefLine } from "../../fileTypes/NMef";
 
 export const createFamillesMetiers = async (
   famillesMetier: Insertable<DB["familleMetier"]>
