0a546358852df24563b93a8458a224470617a6e6
Florian <delacomble@mbp-de-florian-1.home>
2023-09-26 13:43:24 +0200

    feat: one useform for the whole form

--- ui/app/(wrapped)/intentions/intentionForm/InformationsBlock.tsx
+++ ui/app/(wrapped)/intentions/intentionForm/InformationsBlock.tsx
@@ -1,67 +1,34 @@
 import { Box, Button, Flex } from "@chakra-ui/react";
-import { FormProvider, useForm } from "react-hook-form";
 import { ApiType } from "shared";
 
 import { api } from "../../../../api.client";
 import { CapaciteSection } from "./capaciteSection/CapaciteSection";
-import { IntentionForms, PartialIntentionForms } from "./defaultFormValues";
 import { TypeDemandeSection } from "./typeDemandeSection/TypeDemandeSection";
 
 export const InformationsBlock = ({
-  defaultValues,
-  onSubmit,
   isSubmitting,
   onDraftSubmit,
   isDraftSubmitting,
   formMetadata,
 }: {
-  defaultValues: PartialIntentionForms;
-  onSubmit: (values: IntentionForms) => void;
   isSubmitting?: boolean;
-  onDraftSubmit: (values: IntentionForms) => void;
+  onDraftSubmit: () => void;
   isDraftSubmitting?: boolean;
   formMetadata?: ApiType<typeof api.getDemande>["metadata"];
 }) => {
-  const form = useForm<IntentionForms>({
-    defaultValues,
-    mode: "onTouched",
-    reValidateMode: "onChange",
-  });
-  const { handleSubmit } = form;
-
   return (
-    <FormProvider {...form}>
-      <Box
-        noValidate
-        onSubmit={handleSubmit(onSubmit)}
-        bg="white"
-        as="form"
-        p="6"
-        mt="6"
-        mb="6"
-        borderRadius={6}
-      >
-        <TypeDemandeSection
-          formMetadata={formMetadata}
-          defaultValues={defaultValues}
-        />
+    <>
+      <Box bg="white" as="form" p="6" mt="6" mb="6" borderRadius={6}>
+        <TypeDemandeSection formMetadata={formMetadata} />
       </Box>
-      <Box
-        noValidate
-        onSubmit={handleSubmit(onSubmit)}
-        bg="white"
-        as="form"
-        p="6"
-        mt="6"
-        borderRadius={6}
-      >
+      <Box bg="white" as="form" p="6" mt="6" borderRadius={6}>
         <CapaciteSection />
         <Flex justify="flex-end" mt="12" mb="4">
           <Button
             isLoading={isDraftSubmitting}
             variant="secondary"
             mr="4"
-            onClick={() => onDraftSubmit(form.getValues())}
+            onClick={() => onDraftSubmit()}
           >
             Enregistrer en brouillon
           </Button>
@@ -70,6 +37,6 @@ export const InformationsBlock = ({
           </Button>
         </Flex>
       </Box>
-    </FormProvider>
+    </>
   );
 };
--- ui/app/(wrapped)/intentions/intentionForm/IntentionForm.tsx
+++ ui/app/(wrapped)/intentions/intentionForm/IntentionForm.tsx
@@ -3,7 +3,8 @@
 import { Box, Collapse, Container } from "@chakra-ui/react";
 import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
 import { ApiType } from "shared";
 
 import {
@@ -15,8 +16,6 @@ import { api } from "../../../../api.client";
 import { CfdUaiSection } from "./cfdUaiSection/CfdUaiSection";
 import { InformationsBlock } from "./InformationsBlock";
 
-export const UaiRegex = /^[A-Z0-9]{8}$/;
-
 export const IntentionForm = ({
   formId,
   defaultValues,
@@ -26,6 +25,14 @@ export const IntentionForm = ({
   defaultValues: PartialIntentionForms;
   formMetadata?: ApiType<typeof api.getDemande>["metadata"];
 }) => {
+  const form = useForm<IntentionForms>({
+    defaultValues,
+    mode: "onTouched",
+    reValidateMode: "onChange",
+  });
+
+  const { getValues, handleSubmit, watch } = form;
+
   const { isLoading: isSubmitting, mutateAsync: submit } = useMutation({
     mutationFn: ({ forms }: { forms: IntentionForms }) =>
       api
@@ -58,59 +65,59 @@ export const IntentionForm = ({
   const [step, setStep] = useState(
     defaultValues.uai && defaultValues.cfd && defaultValues.dispositifId ? 2 : 1
   );
-  const [intention, setIntention] = useState(defaultValues);
-
-  const submitCfdUai = (values: PartialIntentionForms) => {
-    setIntention({ ...intention, ...values });
-    if (values?.uai && values?.cfd && values?.dispositifId) {
-      setStep(2);
-      return true;
-    }
-  };
 
   const onEditUaiCfdSection = () => setStep(1);
 
   const { push, replace } = useRouter();
 
-  const onSubmit = async (values: IntentionForms) => {
-    const newIntention = {
-      ...intention,
-      ...values,
-    } as IntentionForms;
-    setIntention(newIntention);
+  const onSubmit = async () => {
+    const newIntention = getValues();
     await submit({ forms: newIntention });
     push("/intentions");
   };
 
-  const onDraftSubmit = async (values: PartialIntentionForms) => {
+  const onDraftSubmit = async () => {
     const { id } = await submitDraft({
-      forms: { ...intention, ...values },
+      forms: getValues(),
     });
     replace(id, { scroll: false });
   };
 
+  useEffect(
+    () =>
+      watch(() => {
+        const values = getValues();
+        if (values?.uai && values?.cfd && values?.dispositifId) setStep(2);
+      }).unsubscribe
+  );
+
   return (
-    <Box flex={1} bg="#E2E7F8">
-      <Container maxW={"container.xl"} my={12}>
-        <CfdUaiSection
-          formId={formId}
-          defaultValues={intention}
-          formMetadata={formMetadata}
-          submitCfdUai={submitCfdUai}
-          onEditUaiCfdSection={onEditUaiCfdSection}
-          active={step === 1}
-        />
-        <Collapse in={step === 2} animateOpacity>
-          <InformationsBlock
-            isSubmitting={isSubmitting}
-            onSubmit={onSubmit}
-            isDraftSubmitting={isDraftSubmitting}
-            onDraftSubmit={onDraftSubmit}
-            defaultValues={intention}
+    <FormProvider {...form}>
+      <Box
+        flex={1}
+        bg="#E2E7F8"
+        as="form"
+        noValidate
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <Container maxW={"container.xl"} my={12}>
+          <CfdUaiSection
+            formId={formId}
+            defaultValues={defaultValues}
             formMetadata={formMetadata}
+            onEditUaiCfdSection={onEditUaiCfdSection}
+            active={step === 1}
           />
-        </Collapse>
-      </Container>
-    </Box>
+          <Collapse in={step === 2} animateOpacity>
+            <InformationsBlock
+              isSubmitting={isSubmitting}
+              isDraftSubmitting={isDraftSubmitting}
+              onDraftSubmit={onDraftSubmit}
+              formMetadata={formMetadata}
+            />
+          </Collapse>
+        </Container>
+      </Box>
+    </FormProvider>
   );
 };
