8c260e23f887af422960da201c1ef257b6415cdc
Lucas Détré <lucas.detre@gmail.com>
2023-09-29 15:01:29 +0200

    feat: ajout d'une page de documentation notion

--- ui/app/(wrapped)/documentation/page.tsx
+++ ui/app/(wrapped)/documentation/page.tsx
@@ -1,5 +1,18 @@
-import { redirect } from "next/navigation";
+import { NotionAPI } from "notion-client";
 
-export default () => {
-  redirect("/");
+import { Doc } from "@/app/_components/NotionDoc";
+
+export const revalidate = 60;
+
+const fetchData = async () => {
+  const notion = new NotionAPI();
+  const recordMap = await notion.getPage(
+    "Enregistrer-une-demande-d-ouverture-ou-de-fermeture-dans-Orion-aa580451c49b4e548bb545829de227d4?pvs=4"
+  );
+  return recordMap;
 };
+
+export default async function Documentation() {
+  const recordMap = await fetchData();
+  return <Doc recordMap={recordMap} />;
+}
