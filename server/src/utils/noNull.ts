fe71c816943d7756d7983d39c911340203b15b07
Florian <delacomble@mbp-de-florian-1.home>
2023-09-29 09:18:32 +0200

    feat: deep cleanNull

--- server/src/utils/noNull.ts
+++ server/src/utils/noNull.ts
@@ -1,10 +1,21 @@
-type NoNull<T extends object | undefined | null> = T extends null | undefined
+type NoNull<T extends unknown | undefined | null> = T extends null | undefined
   ? undefined
+  : T extends unknown[]
+  ? ClearArray<T>
+  : // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  T extends (...args: any) => any
+  ? T
   : {
-      [k in keyof T]: T[k] extends Exclude<T[k], null>
-        ? T[k]
-        : Exclude<T[k], null> | undefined;
+      [k in keyof T]: NoNull<T[k]>;
     };
+
+type ClearArray<T extends unknown[]> = T extends [infer F, ...infer R]
+  ? R extends never[]
+    ? [NoNull<F>]
+    : [NoNull<F>, ...ClearArray<R>]
+  : T extends (infer I)[]
+  ? NoNull<I>[]
+  : T;
 
 export const cleanNull = <T extends object | undefined | null>(
   value: T
@@ -12,10 +23,13 @@ export const cleanNull = <T extends object | undefined | null>(
   if (value === undefined || value === null) {
     return undefined as NoNull<T>;
   }
+  if (typeof value !== "object") return value;
+
+  if (Array.isArray(value)) {
+    return value.map(cleanNull) as NoNull<T>;
+  }
+
   return Object.fromEntries(
-    Object.entries(value).map(([key, val]) => [
-      key,
-      val === null ? undefined : val,
-    ])
+    Object.entries(value).map(([key, val]) => [key, cleanNull(val)])
   ) as NoNull<T>;
 };
