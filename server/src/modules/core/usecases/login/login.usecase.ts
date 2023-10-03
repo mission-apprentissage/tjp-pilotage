24f647339500c54d72f8b549921c0c54d0bb2f3a
Florian <delacomble@mbp-de-florian-1.home>
2023-10-03 10:04:53 +0200

    fix: email en minuscule

--- server/src/modules/core/usecases/login/login.usecase.ts
+++ server/src/modules/core/usecases/login/login.usecase.ts
@@ -13,14 +13,15 @@ export const [login, loginFactory] = inject(
   },
   (deps) =>
     async ({ email, password }: { email: string; password: string }) => {
-      const user = await deps.findUserQuery({ email });
+      const formattedEmail = email.toLocaleLowerCase();
+      const user = await deps.findUserQuery({ email: formattedEmail });
       if (!user) throw Boom.unauthorized("wrong credentials");
       if (!user.password) throw Boom.unauthorized("wrong credentials");
 
       const correctPassword = verifyPassword(password, user.password);
       if (!correctPassword) throw Boom.unauthorized("wrong credentials");
 
-      return jwt.sign({ email }, deps.jwtSecret, {
+      return jwt.sign({ email: formattedEmail }, deps.jwtSecret, {
         issuer: "orion",
         expiresIn: "7d",
       });
