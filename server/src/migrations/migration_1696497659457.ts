b214d566893643ca125b1ce3166c61d96d2da3ac
Florian <delacomble@mbp-de-florian-1.home>
2023-10-09 10:43:06 +0200

    feat: colonne cfdContinuum

--- server/src/migrations/migration_1696497659457.ts
+++ server/src/migrations/migration_1696497659457.ts
@@ -6,6 +6,18 @@ export const up = async (db: Kysely<unknown>) => {
     .alterColumn("libelle", (c) => c.setNotNull())
     .alterColumn("codeNiveauDiplome", (c) => c.setNotNull())
     .execute();
+
+  await db.schema
+    .alterTable("indicateurRegionSortie")
+    .addColumn("cfdContinuum", "varchar(8)", (c) =>
+      c.references("formation.codeFormationDiplome")
+    )
+    .execute();
+
+  await db.schema
+    .alterTable("demande")
+    .addForeignKeyConstraint("fk_demande_user", ["createurId"], "user", ["id"])
+    .execute();
 };
 
 export const down = async (db: Kysely<unknown>) => {
@@ -13,5 +25,15 @@ export const down = async (db: Kysely<unknown>) => {
     .alterTable("dataFormation")
     .alterColumn("libelle", (c) => c.dropNotNull())
     .alterColumn("codeNiveauDiplome", (c) => c.dropNotNull())
+    .execute();
+
+  await db.schema
+    .alterTable("indicateurRegionSortie")
+    .dropColumn("cfdContinuum")
+    .execute();
+
+  await db.schema
+    .alterTable("demande")
+    .dropConstraint("fk_demande_user")
     .execute();
 };
