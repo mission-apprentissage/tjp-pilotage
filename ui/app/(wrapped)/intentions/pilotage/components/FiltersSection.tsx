4c10989b4aa6a9a1396a656bf5c04471597c0b0f
Lucas Détré <lucas.detre@gmail.com>
2023-10-23 13:43:16 +0200

    feat: refacto et recette

--- ui/app/(wrapped)/intentions/pilotage/components/FiltersSection.tsx
+++ ui/app/(wrapped)/intentions/pilotage/components/FiltersSection.tsx
@@ -1,21 +1,13 @@
 import { Box, Flex, FormLabel, Select, Skeleton } from "@chakra-ui/react";
 
-import {
-  Filters,
-  PilotageTransformationStats,
-  TerritoiresFilters,
-} from "../types";
+import { PilotageTransformationStats, TerritoiresFilters } from "../types";
 
 export const FiltersSection = ({
-  activeFilters,
-  handleFilters,
   activeTerritoiresFilters,
   handleTerritoiresFilters,
   isLoading,
   data,
 }: {
-  activeFilters: Filters;
-  handleFilters: (type: keyof Filters, value: Filters[keyof Filters]) => void;
   activeTerritoiresFilters: TerritoiresFilters;
   handleTerritoiresFilters: (
     type: keyof TerritoiresFilters,
@@ -40,35 +32,24 @@ export const FiltersSection = ({
         <Box borderRadius={4}>
           <Flex justifyContent={"start"} gap={8} py={3}>
             <Box justifyContent={"start"}>
-              <FormLabel>Rentrées scolaires</FormLabel>
+              <FormLabel>Rentrée scolaire</FormLabel>
               <Select
                 width={"72"}
                 size="md"
                 variant="newInput"
-                value={activeFilters.rentreeScolaire ?? ""}
-                onChange={(e) =>
-                  handleFilters(
-                    "rentreeScolaire",
-                    Number.parseInt(e.target.value)
-                  )
-                }
-                placeholder="TOUTES"
-              >
-                <option value={"2024"}>2024</option>
-                <option value={"2025"}>2025</option>
-                <option value={"2026"}>2026</option>
-                <option value={"2027"}>2027</option>
-              </Select>
+                placeholder="2024"
+                isDisabled={true}
+              ></Select>
             </Box>
             <Box justifyContent={"start"}>
-              <FormLabel>Régions</FormLabel>
+              <FormLabel>Région</FormLabel>
               <Select
                 width={"72"}
                 size="md"
                 variant="newInput"
-                value={activeTerritoiresFilters.codeRegion ?? ""}
+                value={activeTerritoiresFilters.regions ?? ""}
                 onChange={(e) =>
-                  handleTerritoiresFilters("codeRegion", e.target.value)
+                  handleTerritoiresFilters("regions", e.target.value)
                 }
                 placeholder="TOUTES"
               >
@@ -80,14 +61,14 @@ export const FiltersSection = ({
               </Select>
             </Box>
             <Box justifyContent={"start"}>
-              <FormLabel>Académies</FormLabel>
+              <FormLabel>Académie</FormLabel>
               <Select
                 width={"72"}
                 size="md"
                 variant="newInput"
-                value={activeTerritoiresFilters.codeAcademie ?? ""}
+                value={activeTerritoiresFilters.academies ?? ""}
                 onChange={(e) =>
-                  handleTerritoiresFilters("codeAcademie", e.target.value)
+                  handleTerritoiresFilters("academies", e.target.value)
                 }
                 placeholder="TOUTES"
               >
@@ -99,14 +80,14 @@ export const FiltersSection = ({
               </Select>
             </Box>
             <Box justifyContent={"start"}>
-              <FormLabel>Départements</FormLabel>
+              <FormLabel>Département</FormLabel>
               <Select
                 width={"72"}
                 size="md"
                 variant="newInput"
-                value={activeTerritoiresFilters.codeDepartement ?? ""}
+                value={activeTerritoiresFilters.departements ?? ""}
                 onChange={(e) =>
-                  handleTerritoiresFilters("codeDepartement", e.target.value)
+                  handleTerritoiresFilters("departements", e.target.value)
                 }
                 placeholder="TOUS"
               >
--- ui/app/(wrapped)/intentions/pilotage/page.tsx
+++ ui/app/(wrapped)/intentions/pilotage/page.tsx
@@ -2,81 +2,43 @@
 
 import { Box, Container, SimpleGrid } from "@chakra-ui/react";
 import { useQuery } from "@tanstack/react-query";
-import { useRouter, useSearchParams } from "next/navigation";
-import qs from "qs";
 import { useState } from "react";
 
 import { api } from "../../../../api.client";
-import { createParametrizedUrl } from "../../../../utils/createParametrizedUrl";
 import { withAuth } from "../../../../utils/security/withAuth";
 import { CartoSection } from "./components/CartoSection";
 import { FiltersSection } from "./components/FiltersSection";
 import { IndicateursClesSection } from "./components/IndicateursClesSection";
 import { VueOuverturesFermeturesSection } from "./components/VueOuverturesFermeturesSection";
 import { VueTauxTransformationSection } from "./components/VueTauxTransformationSection";
-import { Filters, IndicateurType, Scope, TerritoiresFilters } from "./types";
+import { IndicateurType, Scope, TerritoiresFilters } from "./types";
 
 export default withAuth(
   "restitution-intentions/lecture",
   function PilotageIntentions() {
-    const router = useRouter();
-    const queryParams = useSearchParams();
-    const searchParams: {
-      filters?: Partial<Filters>;
-    } = qs.parse(queryParams.toString());
-
-    const filters = searchParams.filters ?? {};
-    const [scope, setScope] = useState<Scope>();
-    const [scopeCode, setScopeCode] = useState<string | undefined>();
+    const [scope, setScope] = useState<{
+      type: Scope;
+      value: string | undefined;
+    }>();
     const [territoiresFilters, setTerritoiresFilters] = useState<
       Partial<TerritoiresFilters>
     >({});
-
-    const setSearchParams = (params: { filters?: typeof filters }) => {
-      router.replace(
-        createParametrizedUrl(location.pathname, { ...searchParams, ...params })
-      );
-    };
-
-    const handleFilters = (
-      type: keyof Filters,
-      value: Filters[keyof Filters]
-    ) => {
-      setSearchParams({
-        filters: { ...filters, [type]: value },
-      });
-    };
+    const [indicateur, setIndicateur] =
+      useState<IndicateurType>("tauxTransformation");
 
     const handleTerritoiresFilters = (
       type: keyof TerritoiresFilters,
       value: TerritoiresFilters[keyof TerritoiresFilters]
     ) => {
       setTerritoiresFilters({ [type]: value });
-      switch (type) {
-        case "codeRegion":
-          setScope("regions");
-          break;
-        case "codeAcademie":
-          setScope("academies");
-          break;
-        case "codeDepartement":
-          setScope("departements");
-          break;
-        default:
-          break;
-      }
-      setScopeCode(value);
+      setScope({ type, value });
     };
 
     const { data, isLoading: isLoading } = useQuery({
       keepPreviousData: true,
       staleTime: 10000000,
-      queryKey: ["pilotageTransfo", filters],
-      queryFn: api.getTransformationStats({
-        query: {
-          ...filters,
-        },
-      }).call,
+      queryKey: ["pilotageTransfo", scope],
+      queryFn: api.getTransformationStats({}).call,
     });
 
     const indicateurOptions = [
@@ -87,9 +49,6 @@ export default withAuth(
       },
     ];
 
-    const [indicateur, setIndicateur] =
-      useState<IndicateurType>("tauxTransformation");
-
     const handleIndicateurChange = (indicateur: string): void => {
       setIndicateur(indicateur as IndicateurType);
     };
@@ -98,8 +57,6 @@ export default withAuth(
       <Container maxWidth={"100%"} bg="blue.faded">
         <Container maxWidth={"container.xl"} py="4">
           <FiltersSection
-            activeFilters={filters}
-            handleFilters={handleFilters}
             activeTerritoiresFilters={territoiresFilters}
             handleTerritoiresFilters={handleTerritoiresFilters}
             isLoading={isLoading}
@@ -112,7 +69,6 @@ export default withAuth(
                   data={data}
                   isLoading={isLoading}
                   scope={scope}
-                  scopeCode={scopeCode}
                 />
               </Box>
               <CartoSection
@@ -127,12 +83,12 @@ export default withAuth(
               <VueTauxTransformationSection
                 data={data}
                 isLoading={isLoading}
-                codeRegion={territoiresFilters.codeRegion}
+                codeRegion={territoiresFilters.regions}
               />
               <VueOuverturesFermeturesSection
                 data={data}
                 isLoading={isLoading}
-                codeRegion={territoiresFilters.codeRegion}
+                codeRegion={territoiresFilters.regions}
               />
             </SimpleGrid>
           </Box>
