13ea980dd1dbe9d37100b963ca10b8d3b6ae773a
Lucas Détré <lucas.detre@gmail.com>
2023-11-20 13:14:48 +0100

    refactor: data routes

--- server/src/modules/data/index.ts
+++ server/src/modules/data/index.ts
@@ -2,6 +2,7 @@ import { Server } from "../../server";
 import { pilotageReformeRoutes } from "./routes/pilotageReforme.routes";
 import { pilotageTransformationRoutes } from "./routes/pilotageTransformation.routes";
 import { restitutionIntentionsRoutes } from "./routes/restitutionIntentions.routes";
+import { countRestitutionIntentionsStatsRoute } from "./usecases/countRestitutionIntentionsStats/countRestitutionIntentionsStats.route";
 import { getDataForPanoramaDepartementRoute } from "./usecases/getDataForPanoramaDepartement/getDataForPanoramaDepartement.route";
 import { getDataForPanoramaRegionRoute } from "./usecases/getDataForPanoramaRegion/getDataForPanoramaRegion.route";
 import { getDepartementRoute } from "./usecases/getDepartement/getDepartement.route";
@@ -9,8 +10,11 @@ import { getDepartementsRoute } from "./usecases/getDepartements/getDepartements
 import { getEtablissementRoute } from "./usecases/getEtablissement/etablissements.routes";
 import { getEtablissementsRoutes } from "./usecases/getEtablissements/getEtablissements.routes";
 import { getFormationsRoute } from "./usecases/getFormations/formations.routes";
+import { getFormationsTransformationsRoute } from "./usecases/getFormationsTransformationStats/getFormationsTransformations.route";
 import { getRegionRoute } from "./usecases/getRegion/getRegion.route";
 import { getRegionsRoute } from "./usecases/getRegions/getRegions.route";
+import { getRestitutionIntentionsStatsRoute } from "./usecases/getRestitutionIntentionsStats/getRestitutionIntentionStats.route";
+import { getTransformationsStatsRoutes } from "./usecases/getTransformationStats/getTransformationsStats.route";
 
 export const registerFormationModule = ({ server }: { server: Server }) => {
   pilotageReformeRoutes({ server });
@@ -27,5 +31,9 @@ export const registerFormationModule = ({ server }: { server: Server }) => {
     ...getDataForPanoramaRegionRoute({ server }),
     ...getRegionRoute({ server }),
     ...getRegionsRoute({ server }),
+    ...getFormationsTransformationsRoute({ server }),
+    ...getTransformationsStatsRoutes({ server }),
+    ...getRestitutionIntentionsStatsRoute({ server }),
+    ...countRestitutionIntentionsStatsRoute({ server }),
   };
 };
--- server/src/modules/data/routes/pilotageTransformation.routes.ts
+++ server/src/modules/data/routes/pilotageTransformation.routes.ts
@@ -2,7 +2,6 @@ import { ROUTES_CONFIG } from "shared";
 
 import { Server } from "../../../server";
 import { hasPermissionHandler } from "../../core";
-import { getFormationsTransformationStats } from "../usecases/getFormationsTransformationStats/getFormationsTransformationStats.usecase";
 import { getTransformationStats } from "../usecases/getTransformationStats/getTransformationStats.usecase";
 
 export const pilotageTransformationRoutes = ({
@@ -20,22 +19,6 @@ export const pilotageTransformationRoutes = ({
       const { order, orderBy, ...filters } = request.query;
 
       const stats = await getTransformationStats({
-        ...filters,
-        orderBy: order && orderBy ? { order, column: orderBy } : undefined,
-      });
-      response.status(200).send(stats);
-    }
-  );
-
-  server.get(
-    "/pilotage-transformation/formations",
-    {
-      schema: ROUTES_CONFIG.getFormationsTransformationStats,
-      preHandler: hasPermissionHandler("pilotage-intentions/lecture"),
-    },
-    async (request, response) => {
-      const { order, orderBy, ...filters } = request.query;
-      const stats = await getFormationsTransformationStats({
         ...filters,
         orderBy: order && orderBy ? { order, column: orderBy } : undefined,
       });
