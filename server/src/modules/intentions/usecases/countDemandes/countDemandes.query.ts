566776dc148833a56310dfb6e1b0f0d815b219e2
Florian <delacomble@mbp-de-florian.home>
2023-11-27 16:46:23 +0100

    fix count types

--- server/src/modules/intentions/usecases/countDemandes/countDemandes.query.ts
+++ server/src/modules/intentions/usecases/countDemandes/countDemandes.query.ts
@@ -11,14 +11,14 @@ export const countDemandes = async ({
 }) => {
   const countDemandes = await kdb
     .selectFrom("demande")
-    .select((eb) => sql<string>`count(${eb.ref("demande.id")})`.as("total"))
+    .select((eb) => sql<number>`count(${eb.ref("demande.id")})`.as("total"))
     .select((eb) =>
-      sql<string>`SUM(CASE WHEN ${eb.ref(
+      sql<number>`SUM(CASE WHEN ${eb.ref(
         "demande.status"
       )} = 'draft' THEN 1 ELSE 0 END)`.as("draft")
     )
     .select((eb) =>
-      sql<string>`SUM(CASE WHEN ${eb.ref(
+      sql<number>`SUM(CASE WHEN ${eb.ref(
         "demande.status"
       )} = 'submitted' THEN 1 ELSE 0 END)`.as("submitted")
     )
