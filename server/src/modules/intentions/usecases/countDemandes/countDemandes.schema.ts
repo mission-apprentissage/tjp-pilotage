59b5911bfdd5028f7cf4dcb5bb4f57e14a7e4c02
Florian <delacomble@mbp-de-florian.home>
2023-11-27 16:46:23 +0100

    fix count types

--- server/src/modules/intentions/usecases/countDemandes/countDemandes.schema.ts
+++ server/src/modules/intentions/usecases/countDemandes/countDemandes.schema.ts
@@ -3,9 +3,9 @@ import { z } from "zod";
 export const countDemandesSchema = {
   response: {
     200: z.object({
-      total: z.string(),
-      draft: z.string(),
-      submitted: z.string(),
+      total: z.number(),
+      draft: z.number(),
+      submitted: z.number(),
     }),
   },
 };
