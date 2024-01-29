b6559ef32fb18903b94f238bd098e9e47b44be9a
Marin <marin.procureur@gmail.com>
2024-01-29 18:01:03 +0100

    feat: Page changelog + bandeau notification utilisateur

---
+++ ui/app/(wrapped)/components/InformationHeader.tsx
@@ -0,0 +1,101 @@
+import { Box, Button, Link, Stack, Text, VStack } from "@chakra-ui/react";
+import { Icon } from "@iconify/react";
+import NextLink from "next/link";
+import { useEffect, useState } from "react";
+
+import { useChangelog } from "../changelog/useChangelog";
+
+const LOCAL_STORAGE_KEY = "closedChangelogEntries";
+
+export default function InformationHeader() {
+  const [closedEntries, setClosedEntries] = useState<Array<string>>(
+    JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) ?? "[]")
+  );
+  const { changelog } = useChangelog();
+
+  const filteredChangelog =
+    changelog?.filter(
+      (changelogEntry) =>
+        changelogEntry.types.findIndex((t) => t.label === "BANDEAU") !== -1 &&
+        changelogEntry.show &&
+        !closedEntries.includes(changelogEntry.id)
+    ) ?? [];
+
+  useEffect(() => {
+    if (closedEntries.length > 0) {
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(closedEntries));
+    }
+  }, [closedEntries]);
+
+  function closeInfo(id: string) {
+    if (!closedEntries.includes(id)) {
+      setClosedEntries([...closedEntries, id]);
+    }
+  }
+
+  return (
+    <VStack>
+      {filteredChangelog.map((changelogEntry) => (
+        <Box
+          backgroundColor="#E8EDFF"
+          color="#0063CB"
+          width="100%"
+          paddingY="12px"
+          key={`${changelogEntry.title}`}
+        >
+          <Stack
+            maxWidth={"container.xl"}
+            margin="auto"
+            display="flex"
+            flexDirection="row"
+            justifyContent="space-between"
+            flexWrap="nowrap"
+            spacing="16px"
+            padding="4px 8px"
+          >
+            <Icon icon="ri:information-fill" fontSize="24px" />
+            <Text
+              flexGrow={1}
+              fontSize="16px"
+              fontWeight={700}
+              display={{
+                base: "none",
+                md: "block",
+              }}
+            >
+              {changelogEntry.description}
+              <Link as={NextLink} href="/changelog" textDecoration="underline">
+                Voir les détails
+              </Link>
+            </Text>
+            <Text
+              fontSize="12px"
+              display={{
+                base: "block",
+                md: "none",
+              }}
+            >
+              Une nouvelle mise à jour a été déployée.{" "}
+              <Link as={NextLink} href="/changelog" textDecoration="underline">
+                Voir les détails
+              </Link>
+            </Text>
+            <Button
+              onClick={() => closeInfo(changelogEntry.id)}
+              variant="inline"
+              display="flex"
+              padding="0"
+              flexDirection="row"
+              justifyItems="end"
+              alignItems="start"
+              width="auto"
+              height="auto"
+            >
+              <Icon icon="ri:close-fill" fontSize="16px" />
+            </Button>
+          </Stack>
+        </Box>
+      ))}
+    </VStack>
+  );
+}
--- ui/app/_components/NotionDoc.tsx
+++ ui/app/_components/NotionDoc.tsx
@@ -1,17 +1,20 @@
 "use client";
+import { Box } from "@chakra-ui/react";
 import { ExtendedRecordMap } from "notion-types";
 import { Suspense } from "react";
 import { NotionRenderer } from "react-notion-x";
 export const Doc = ({ recordMap }: { recordMap: ExtendedRecordMap }) => {
   return (
     <Suspense>
-      <NotionRenderer
-        pageTitle={false}
-        disableHeader={true}
-        recordMap={recordMap}
-        fullPage={true}
-        darkMode={false}
-      />
+      <Box>
+        <NotionRenderer
+          pageTitle={false}
+          disableHeader={true}
+          recordMap={recordMap}
+          fullPage={true}
+          darkMode={false}
+        />
+      </Box>
     </Suspense>
   );
 };
