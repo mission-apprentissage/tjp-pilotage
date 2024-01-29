17fe7da0e2085dfeacb69261834dbb164061f51f
Gaetan BUSATO <gaetan.busato@gmail.com>
2024-01-29 11:43:39 +0100

    feat(glossaire): ajout d'un cache memoire au glossaire

--- server/src/modules/glossaire/usecases/getGlossaireEntry/getGlossaireEntry.route.ts
+++ server/src/modules/glossaire/usecases/getGlossaireEntry/getGlossaireEntry.route.ts
@@ -1,10 +1,21 @@
 import { createRoute } from "@http-wizard/core";
+import NodeCache from "node-cache";
 
 import { Server } from "../../../../server";
-import { getGlossaireEntrySchema } from "./getGlossaireEntry.schema";
+import { CACHE_KEYS } from "../../../../utils/cacheKeys";
+import {
+  getGlossaireEntrySchema,
+  GlossaireEntry,
+} from "./getGlossaireEntry.schema";
 import { getGlossaireEntry } from "./getGlossaireEntry.usecase";
 
-export const getGlossaireEntryRoute = ({ server }: { server: Server }) => {
+export const getGlossaireEntryRoute = ({
+  server,
+  cache,
+}: {
+  server: Server;
+  cache: NodeCache;
+}) => {
   return createRoute("/glossaire/:id", {
     method: "GET",
     schema: getGlossaireEntrySchema,
@@ -12,7 +23,19 @@ export const getGlossaireEntryRoute = ({ server }: { server: Server }) => {
     server.route({
       ...props,
       handler: async (request, response) => {
+        if (cache.has(CACHE_KEYS.GLOSSAIRE_ENTRY(request.params.id))) {
+          const glossaireEntry = cache.take<GlossaireEntry>(
+            CACHE_KEYS.GLOSSAIRE_ENTRY(request.params.id)
+          );
+          response.status(200).send(glossaireEntry);
+          return;
+        }
+
         const glossaireEntry = await getGlossaireEntry(request.params.id);
+        cache.set(
+          CACHE_KEYS.GLOSSAIRE_ENTRY(request.params.id),
+          glossaireEntry
+        );
         response.status(200).send(glossaireEntry);
       },
     });
