0b697ede293e91164a14d16850f555f5e50df1a5
Gaetan BUSATO <gaetan.busato@gmail.com>
2024-01-29 11:43:39 +0100

    feat(glossaire): ajout d'un cache memoire au glossaire

--- server/src/modules/glossaire/usecases/getGlossaire/getGlossaire.route.ts
+++ server/src/modules/glossaire/usecases/getGlossaire/getGlossaire.route.ts
@@ -1,10 +1,18 @@
 import { createRoute } from "@http-wizard/core";
+import NodeCache from "node-cache";
 
 import { Server } from "../../../../server";
-import { getGlossaireSchema } from "./getGlossaire.schema";
+import { CACHE_KEYS } from "../../../../utils/cacheKeys";
+import { getGlossaireSchema,GlossaireEntry } from "./getGlossaire.schema";
 import { getGlossaire } from "./getGlossaire.usecase";
 
-export const getGlossaireRoute = ({ server }: { server: Server }) => {
+export const getGlossaireRoute = ({
+  server,
+  cache,
+}: {
+  server: Server;
+  cache: NodeCache;
+}) => {
   return createRoute("/glossaire", {
     method: "GET",
     schema: getGlossaireSchema,
@@ -12,7 +20,14 @@ export const getGlossaireRoute = ({ server }: { server: Server }) => {
     server.route({
       ...props,
       handler: async (_, response) => {
+        if (cache.has(CACHE_KEYS.GLOSSAIRE)) {
+          const glossaire = cache.take<GlossaireEntry[]>(CACHE_KEYS.GLOSSAIRE);
+          response.status(200).send(glossaire);
+          return;
+        }
+
         const glossaire = await getGlossaire();
+        cache.set(CACHE_KEYS.GLOSSAIRE, glossaire);
         response.status(200).send(glossaire);
       },
     });
