210b29539ef39d219b9cbba8b9aca0d77c7feab0
Gaetan BUSATO <gaetan.busato@gmail.com>
2024-02-29 13:25:56 +0100

    feat: header etablissement

--- server/src/modules/data/index.ts
+++ server/src/modules/data/index.ts
@@ -7,6 +7,7 @@ import { getDataForPanoramaRegionRoute } from "./usecases/getDataForPanoramaRegi
 import { getDepartementRoute } from "./usecases/getDepartement/getDepartement.route";
 import { getDepartementsRoute } from "./usecases/getDepartements/getDepartements.route";
 import { getEtablissementRoute } from "./usecases/getEtablissement/getEtablissement.route";
+import { getEtablissementHeaderRoute } from "./usecases/getEtablissement/header/getEtablissementHeader.route";
 import { getFormationEtablissementsRoutes } from "./usecases/getFormationEtablissements/getFormationEtablissements.routes";
 import { getFormationsRoute } from "./usecases/getFormations/getFormations.routes";
 import { getFormationsTransformationsRoute } from "./usecases/getFormationsTransformationStats/getFormationsTransformations.route";
@@ -21,7 +22,8 @@ import { searchEtablissementRoute } from "./usecases/searchEtablissement/searchE
 export const registerFormationModule = ({ server }: { server: Server }) => {
   return {
     ...getFormationsRoute({ server }),
-    ...getEtablissementRoute(server),
+    ...getEtablissementHeaderRoute({ server }),
+    ...getEtablissementRoute({ server }),
     ...getFormationEtablissementsRoutes({ server }),
     ...getDataForPanoramaEtablissementRoute({ server }),
     ...getDepartementRoute({ server }),
--- ui/app/(wrapped)/etablissement/components/header/index.tsx
+++ ui/app/(wrapped)/etablissement/components/header/index.tsx
@@ -1,5 +1,28 @@
-const EtablissementHeader = () => {
-  return <></>;
-};
+import { Grid } from "@chakra-ui/react";
 
-export { EtablissementHeader };
+import { useEtablissementContext } from "../../context/etablissementContext";
+import { AccesRapide } from "./accesRapide";
+import { Coordonnees } from "./coordonnees";
+import { DonneesIncompletes } from "./donneesIncompletes";
+import { Filieres } from "./filieres";
+import { useEtablissementHeader } from "./hook";
+import { Indicateurs } from "./indicateurs";
+import { Libelle } from "./libelle";
+import { SearchInput } from "./searchInput";
+
+export const EtablissementHeader = () => {
+  const { uai } = useEtablissementContext();
+  const { filieres, informations, search } = useEtablissementHeader(uai) || {};
+
+  return (
+    <Grid templateColumns={"repeat(12,1fr)"}>
+      <Libelle informations={informations} />
+      <SearchInput search={search} />
+      <Coordonnees informations={informations} />
+      <Filieres filieres={filieres} />
+      <AccesRapide uai={uai} />
+      <Indicateurs millessimes={["2022", "2023"]} />
+      <DonneesIncompletes isMissingDatas={true} />
+    </Grid>
+  );
+};
