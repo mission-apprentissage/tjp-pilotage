221675258800a54f99a86f1db4a920230726495c
Lucas Détré <lucas.detre@gmail.com>
2024-03-01 01:30:08 +0100

    fix: truncate diplome pro avant import

--- server/src/migrations/migration_1709251571201.ts
+++
@@ -1,9 +0,0 @@
-import { Kysely, sql } from "kysely";
-
-export const up = async (db: Kysely<unknown>) => {
-  await db.executeQuery(sql`TRUNCATE TABLE "diplomeProfessionnel" CASCADE;`.compile(db));
-};
-
-export const down = async (_db: Kysely<unknown>) => {
-  // No down migration
-};
