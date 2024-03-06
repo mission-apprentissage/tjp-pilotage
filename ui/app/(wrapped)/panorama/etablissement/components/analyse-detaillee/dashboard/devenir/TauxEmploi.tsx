631ed0f7ba61cb871197562033267692eab39e75
Lucas Détré <lucas.detre@gmail.com>
2024-03-06 20:01:13 +0100

    feat: remplacement du panorama établissement

--- ui/app/(wrapped)/panorama/etablissement/components/analyse-detaillee/dashboard/devenir/TauxEmploi.tsx
+++ ui/app/(wrapped)/panorama/etablissement/components/analyse-detaillee/dashboard/devenir/TauxEmploi.tsx
@@ -1,16 +1,13 @@
-import { VerticalBarChart } from "@/app/(wrapped)/etablissement/components/analyse-detaillee/components/VerticalBarChart";
-import {
-  formatMillesime,
-  formatTaux,
-} from "@/app/(wrapped)/etablissement/components/analyse-detaillee/formatData";
-import { ChiffresIJOffre } from "@/app/(wrapped)/etablissement/components/analyse-detaillee/types";
 import { useGlossaireContext } from "@/app/(wrapped)/glossaire/glossaireContext";
 import { TooltipIcon } from "@/components/TooltipIcon";
 
 import { DashboardCard } from "../../../DashboardCard";
 import { CounterChart } from "../../components/CounterChart";
+import { VerticalBarChart } from "../../components/VerticalBarChart";
+import { formatMillesime, formatTaux } from "../../formatData";
+import { ChiffresIJOffre } from "../../types";
 
-export const TauxPoursuiteEtudes = ({
+export const TauxEmploi = ({
   chiffresIJOffre,
 }: {
   chiffresIJOffre?: ChiffresIJOffre;
@@ -19,28 +16,28 @@ export const TauxPoursuiteEtudes = ({
   const checkDataAvailability = (chiffresIJOffre: ChiffresIJOffre) => {
     return (
       Object.values(chiffresIJOffre).findIndex(
-        (value) => value.tauxPoursuite
+        (value) => value.tauxInsertion
       ) !== -1
     );
   };
   return (
     <DashboardCard
-      label={"Poursuite d'études"}
+      label="Taux d'emploi à 6 mois"
       tooltip={
         <TooltipIcon
           ml="1"
-          label="Poursuite d'études"
-          onClick={() => openGlossaire("taux-poursuite-etudes")}
+          label="Taux d'emploi à 6 mois après la formation"
+          onClick={() => openGlossaire("taux-emploi-6-mois")}
         />
       }
     >
       {chiffresIJOffre && checkDataAvailability(chiffresIJOffre) ? (
         <VerticalBarChart
           data={Object.keys(chiffresIJOffre)
-            .filter((millesime) => chiffresIJOffre[millesime].tauxPoursuite)
+            .filter((millesime) => chiffresIJOffre[millesime].tauxInsertion)
             .map((millesime) => ({
               label: formatMillesime(millesime),
-              value: formatTaux(chiffresIJOffre[millesime].tauxPoursuite),
+              value: formatTaux(chiffresIJOffre[millesime].tauxInsertion),
             }))}
         />
       ) : (
