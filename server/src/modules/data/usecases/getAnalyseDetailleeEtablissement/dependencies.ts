53396389896b7e6bfaa24705f2d103039d0269e3
Lucas Détré <lucas.detre@gmail.com>
2024-03-06 10:45:26 +0100

    fix: première recette v2 wip

--- server/src/modules/data/usecases/getAnalyseDetailleeEtablissement/dependencies.ts
+++ server/src/modules/data/usecases/getAnalyseDetailleeEtablissement/dependencies.ts
@@ -27,10 +27,10 @@ import { selectTauxRemplissage } from "../../utils/tauxRemplissage";
 
 const getBase = ({
   uai,
-  _rentreeScolaire = CURRENT_RENTREE,
+  rentreeScolaire = CURRENT_RENTREE,
 }: {
   uai: string;
-  _rentreeScolaire?: string;
+  rentreeScolaire?: string;
 }) =>
   kdb
     .selectFrom("formationEtablissement")
@@ -58,17 +58,21 @@ const getBase = ({
         )
         .on("formationEtablissement.voie", "=", "scolaire")
     )
-    .leftJoin("indicateurEntree as ie", (join) =>
+    .leftJoin("indicateurEntree", (join) =>
       join
-        .onRef("ie.formationEtablissementId", "=", "formationEtablissement.id")
+        .onRef(
+          "indicateurEntree.formationEtablissementId",
+          "=",
+          "formationEtablissement.id"
+        )
         .on("formationEtablissement.voie", "=", "scolaire")
     )
     .where((w) =>
       w.and([
-        // w.or([
-        //   w("ie.rentreeScolaire", "=", rentreeScolaire),
-        //   w("ie.rentreeScolaire", "is", null),
-        // ]),
+        w.or([
+          w("indicateurEntree.rentreeScolaire", "=", rentreeScolaire),
+          w("indicateurEntree.rentreeScolaire", "is", null),
+        ]),
         w("formationEtablissement.UAI", "=", uai),
       ])
     );
@@ -206,6 +210,13 @@ const getChiffresEntree = async ({
   const eb2 = expressionBuilder<DB, keyof DB>();
 
   return getBase({ uai })
+    .innerJoin("indicateurEntree as ie", (join) =>
+      join.onRef(
+        "ie.formationEtablissementId",
+        "=",
+        "formationEtablissement.id"
+      )
+    )
     .where("ie.rentreeScolaire", "in", [
       rentreeScolaire,
       getRentreeScolaire({ rentreeScolaire, offset: -1 }),
