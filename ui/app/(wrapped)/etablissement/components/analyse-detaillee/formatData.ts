a733f377d953d39861336847bf382addb2e59770
Gaetan BUSATO <gaetan.busato@gmail.com>
2024-03-07 09:52:55 +0100

    feat: suppression de la page etablissement de test

--- ui/app/(wrapped)/etablissement/components/analyse-detaillee/formatData.ts
+++
@@ -1,8 +0,0 @@
-export const formatTaux = (value?: number): number =>
-  Number(((value ?? 0) * 100).toFixed(0));
-
-export const formatAbsolute = (value?: number): number =>
-  Number((value ?? 0).toFixed(2));
-
-export const formatMillesime = (millesime: string): string =>
-  `${millesime.split("_")[0]}+${millesime.split("_")[1].substring(2)}`;
--- ui/app/(wrapped)/etablissement/components/header/types.ts
+++
@@ -1,8 +0,0 @@
-import { client } from "../../../../../api.client";
-
-export type EtablissementHeader =
-  (typeof client.infer)["[GET]/etablissement/:uai/header"];
-export type Informations = EtablissementHeader["informations"];
-export type Nsfs = EtablissementHeader["nsfs"];
-export type Indicateurs = EtablissementHeader["indicateurs"];
-export type Indicateur = EtablissementHeader["indicateurs"]["tauxPoursuite"];
