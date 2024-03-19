23ed13b72bbfa35bf937370c19a935c27c21dcdf
Gaetan BUSATO <gaetan.busato@gmail.com>
2024-03-19 09:51:02 +0100

    feat: mise à jour du composant CounterChart pour correspondre aux besoins sur Firefox

--- ui/app/(wrapped)/panorama/etablissement/components/analyse-detaillee/components/CounterChart.tsx
+++ ui/app/(wrapped)/panorama/etablissement/components/analyse-detaillee/components/CounterChart.tsx
@@ -9,42 +9,29 @@ export const CounterChart = ({
   compareData?: React.ReactNode;
   type?: "absolute" | "percentage";
 }) => {
+  if (typeof data === "undefined") {
+    return (
+      <Text fontSize={"12px"} fontWeight={"400"} color={"grey.425"}>
+        Indisponible
+      </Text>
+    );
+  }
+
   return (
     <Flex justify={"space-between"} direction={"row"} width={"100%"}>
-      {data ? (
-        <>
-          <Flex mb="unset" mt="auto" align={"flex-end"}>
-            <Text fontSize={"36px"} lineHeight={"36px"} fontWeight={"700"}>
-              {data}
-            </Text>
-            {type === "percentage" && (
-              <Text fontSize={"14px"} fontWeight={"400"}>
-                &nbsp;%
-              </Text>
-            )}
-          </Flex>
-          <Flex
-            shrink={0}
-            justify={"end"}
-            direction={"column"}
-            mb="unset"
-            fontSize={"12px"}
-          >
-            <Flex>{compareData}</Flex>
-          </Flex>
-        </>
-      ) : (
-        <>
-          <Flex
-            fontSize={"11px"}
-            lineHeight={"14px"}
-            fontWeight={"400"}
-            color={"grey.425"}
-          >
-            Indisponible
-          </Flex>
-        </>
-      )}
+      <Flex align={"flex-end"}>
+        <Text fontSize={"36px"} lineHeight={"36px"} fontWeight={"700"}>
+          {data}
+        </Text>
+        {type === "percentage" && (
+          <Text fontSize={"14px"} fontWeight={"400"}>
+            &nbsp;%
+          </Text>
+        )}
+      </Flex>
+      <Flex align={"flex-end"} fontSize={"12px"}>
+        {compareData}
+      </Flex>
     </Flex>
   );
 };
