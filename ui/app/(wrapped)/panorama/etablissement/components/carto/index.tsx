3a41696956b0c33506a41f82ac20f9eb61e402d9
Marin <marin.procureur@gmail.com>
2024-03-27 17:40:58 +0100

    feat (carto): Ajout d'un bouton centrer sur l'établissement

--- ui/app/(wrapped)/panorama/etablissement/components/carto/index.tsx
+++ ui/app/(wrapped)/panorama/etablissement/components/carto/index.tsx
@@ -1,6 +1,7 @@
-import { Divider, Grid, Stack, Text } from "@chakra-ui/react";
+import { Box, Divider, Grid, HStack, Stack, Text } from "@chakra-ui/react";
 
 import { useEtablissementContext } from "../../context/etablissementContext";
+import { CenterMap } from "./components/CenterMap";
 import { CfdSelect } from "./components/CfdSelect";
 import { ListeEtablissementsProches } from "./components/ListEtablissementsProches";
 import { Map } from "./components/Map";
@@ -20,7 +21,17 @@ export const EtablissementMap = () => {
           Visualisez l'offre de formations sur la carte régionale, et comparez
           les établissements selon les taux d'emploi et de devenir favorable.
         </Text>
-        <CfdSelect />
+        <HStack
+          gap="16px"
+          alignItems="end"
+          justifyContent="space-between"
+          width="100%"
+        >
+          <Box flexGrow={1}>
+            <CfdSelect />
+          </Box>
+          <CenterMap />
+        </HStack>
         <Grid
           templateColumns={"repeat(2,50%)"}
           templateRows={"repeat(1,500px)"}
