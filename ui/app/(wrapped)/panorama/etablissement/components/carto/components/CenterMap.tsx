b792d6565d032b2d09f94cf2cce703cf9299b9d9
Marin <marin.procureur@gmail.com>
2024-03-27 17:40:58 +0100

    feat (carto): Ajout d'un bouton centrer sur l'établissement

---
+++ ui/app/(wrapped)/panorama/etablissement/components/carto/components/CenterMap.tsx
@@ -0,0 +1,35 @@
+import { Button, HStack, Text } from "@chakra-ui/react";
+import { Icon } from "@iconify/react";
+
+import { useEtablissementMapContext } from "../context/etablissementMapContext";
+
+export const CenterMap = () => {
+  const { map, etablissementMap } = useEtablissementMapContext();
+
+  const etablissement = etablissementMap?.etablissement;
+
+  const flyToHome = () => {
+    if (
+      etablissementMap !== undefined &&
+      map !== undefined &&
+      etablissement !== undefined
+    ) {
+      map.flyTo({
+        center: {
+          lng: etablissement.longitude,
+          lat: etablissement.latitude,
+        },
+        zoom: etablissementMap.initialZoom,
+      });
+    }
+  };
+
+  return (
+    <Button variant="primary" onClick={() => flyToHome()}>
+      <HStack>
+        <Icon icon="ri:map-pin-line" />
+        <Text>Recentrer sur l'établissement</Text>
+      </HStack>
+    </Button>
+  );
+};
