46ee91b6279514883a0630000430f93d6cc1f225
Lucas Détré <lucas.detre@gmail.com>
2024-04-25 13:42:27 +0200

    feat: ajustement UI sticky filtre panorama

---
+++ ui/app/(wrapped)/utils/getStickyNavOffset.ts
@@ -0,0 +1,10 @@
+const NAV_HEIGHT = 52;
+const OFFSET = 16;
+
+export const getStickyNavHeightWithOffset = () => {
+  return `${NAV_HEIGHT + OFFSET}px`;
+};
+
+export const getStickyNavHeight = () => {
+  return `${NAV_HEIGHT}px`;
+};
