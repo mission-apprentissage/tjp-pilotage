197930bf169feb474eb3c6e968a0d75fc7c469a1
Gaetan BUSATO <gaetan.busato@gmail.com>
2024-05-13 21:00:03 +0200

    feat: ajout du système de fichiers core

--- server/config/config.ts
+++ server/config/config.ts
@@ -78,4 +78,11 @@ export const config = {
     dbChangelogId: env.get("NOTION_DB_CHANGELOG_ID").required().asString(),
     dbGlossaireId: env.get("NOTION_DB_GLOSSAIRE_ID").required().asString(),
   },
+  s3: {
+    region: env.get("PILOTAGE_S3_REGION").asString(),
+    endpoint: env.get("PILOTAGE_S3_ENDPOINT").asString(),
+    bucket: env.get("PILOTAGE_S3_BUCKET").asString(),
+    accessKey: env.get("PILOTAGE_S3_ACCESS_KEY").asString(),
+    secretKey: env.get("PILOTAGE_S3_SECRET_KEY").asString(),
+  },
 };
