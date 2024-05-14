e47a627c6c4ff151f6e35868dcc002bbceac435e
Gaetan BUSATO <gaetan.busato@gmail.com>
2024-05-14 09:58:00 +0200

    feat: ajout de la gestion de l'upload des fichiers coté front

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,14 @@
+import path from "node:path";
+
 import fastifyCors from "@fastify/cors";
+import fastifyMultipart from "@fastify/multipart";
+import fastifyStatic from "@fastify/static";
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUi from "@fastify/swagger-ui";
 import Boom from "@hapi/boom";
 import * as Sentry from "@sentry/node";
 import { jsonSchemaTransform } from "fastify-type-provider-zod";
+import { MAX_FILE_SIZE, MAX_FILE_SIZE_IN_MB } from "shared";
 import { EnvEnum } from "shared/enum/envEnum";
 import { ZodError } from "zod";
 
@@ -50,6 +55,17 @@ server.register(fastifySwaggerUi, {
   },
 });
 
+server.register(fastifyMultipart, {
+  limits: { fileSize: MAX_FILE_SIZE },
+});
+
+if (config.env === EnvEnum.dev) {
+  server.register(fastifyStatic, {
+    root: path.join(__dirname, "../public"),
+    prefix: "/public/",
+  });
+}
+
 server.setErrorHandler((error, _, reply) => {
   Sentry.captureException(error);
 
@@ -69,6 +85,15 @@ server.setErrorHandler((error, _, reply) => {
     if (error.output.statusCode >= 500) {
       logger.error(error.message, { error, data: error.data });
     }
+    return;
+  }
+
+  if (error.statusCode === 413) {
+    reply.status(413).send({
+      error: "Payload Too Large",
+      message: `The max allowed payload size is of ${MAX_FILE_SIZE_IN_MB} MB`,
+      statusCode: 413,
+    });
     return;
   }
 
--- ui/app/(wrapped)/intentions/perdir/saisie/[numero]/client.tsx
+++ ui/app/(wrapped)/intentions/perdir/saisie/[numero]/client.tsx
@@ -4,6 +4,7 @@ import { client } from "@/api.client";
 
 import { IntentionSpinner } from "../components/IntentionSpinner";
 import { IntentionForm } from "../intentionForm/IntentionForm";
+import { IntentionFilesProvider } from "../intentionForm/observationsSection/filesSection/filesContext";
 import { isSaisieDisabled } from "../utils/isSaisieDisabled";
 
 export default ({
@@ -24,13 +25,15 @@ export default ({
   return (
     <>
       {data && (
-        <IntentionForm
-          disabled={!data.canEdit || isSaisieDisabled()}
-          formId={numero}
-          defaultValues={data}
-          formMetadata={data.metadata}
-          campagne={data.campagne}
-        />
+        <IntentionFilesProvider numero={numero}>
+          <IntentionForm
+            disabled={!data.canEdit || isSaisieDisabled()}
+            formId={numero}
+            defaultValues={data}
+            formMetadata={data.metadata}
+            campagne={data.campagne}
+          />
+        </IntentionFilesProvider>
       )}
     </>
   );
