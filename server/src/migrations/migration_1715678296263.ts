81b0c4e4e3c3ec432a5ce30f48d83bcecaf75b81
Lucas Détré <lucas.detre@gmail.com>
2024-05-14 11:29:11 +0200

    feat: modification source discipline

---
+++ server/src/migrations/migration_1715678296263.ts
@@ -0,0 +1,19 @@
+import { Kysely, sql } from "kysely";
+
+export const up = async (db: Kysely<unknown>) => {
+  await db.executeQuery(sql`TRUNCATE TABLE "discipline" CASCADE;`.compile(db));
+
+  await db.schema
+    .alterTable("discipline")
+    .dropColumn("dateOuverture")
+    .dropColumn("dateFermeture")
+    .execute();
+};
+
+export const down = async (db: Kysely<unknown>) => {
+  await db.schema
+    .alterTable("discipline")
+    .addColumn("dateOuverture", "date")
+    .addColumn("dateFermeture", "date")
+    .execute();
+};
