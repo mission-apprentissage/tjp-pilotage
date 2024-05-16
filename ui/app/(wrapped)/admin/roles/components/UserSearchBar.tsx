41a4b35b1658bc46ad8621521cebd90911496202
Marin <marin.procureur@gmail.com>
2024-05-16 18:10:10 +0200

    feat (page-roles): Initialisation de la page rôles

---
+++ ui/app/(wrapped)/admin/roles/components/UserSearchBar.tsx
@@ -0,0 +1,119 @@
+import { Badge, HStack, Skeleton, Text } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { components, CSSObjectWithLabel, SingleValueProps } from "react-select";
+import AsyncSelect from "react-select/async";
+
+import { client } from "../../../../../api.client";
+import { useAuth } from "../../../../../utils/security/useAuth";
+import { ROLES_LABELS } from "../const";
+
+export type QueryResult = (typeof client.infer)["[GET]/user/search/:search"];
+export type User = QueryResult[number];
+type OptionType = {
+  label: string;
+  value: User;
+};
+
+const SingleValue = ({ ...props }: SingleValueProps<OptionType>) => {
+  const role = props.getValue()[0].value.role!;
+  const firstName = props.getValue()[0].value.firstname!;
+  const lastName = props.getValue()[0].value.lastname!;
+
+  return (
+    <components.SingleValue {...props}>
+      <HStack>
+        <Text>
+          {firstName} {lastName}{" "}
+        </Text>
+        <Badge variant="info" size="sm">
+          {ROLES_LABELS[role].label}
+        </Badge>
+      </HStack>
+    </components.SingleValue>
+  );
+};
+
+interface UserSearchBarProps {
+  updateUser: (user: User) => void;
+  user: User | undefined;
+}
+
+const UserSearchBar = ({ updateUser, user }: UserSearchBarProps) => {
+  const { auth } = useAuth();
+
+  const { data: defaultValues, isLoading } = client
+    .ref("[GET]/user/search/:search")
+    .useQuery({ params: { search: auth!.user!.email } });
+
+  useEffect(() => {
+    if (!isLoading && defaultValues) {
+      // TODO: Fix User type.
+      // Le cast est nécessaire ici puisque le type utilisateur peut ne pas avoir de rôle ou de codeRegion
+      // Ce qui n'est pas effectivement le cas en production. Le rôle est requis.
+      updateUser({
+        ...defaultValues[0],
+        role: defaultValues[0].role!,
+      });
+    }
+  }, [isLoading, defaultValues]);
+
+  return (
+    <>
+      {(!user || isLoading) && <Skeleton width="200px" height="24px" />}
+      {user && !isLoading && (
+        <AsyncSelect
+          instanceId={"user-search-email"}
+          styles={{
+            control: (styles: CSSObjectWithLabel) => ({
+              ...styles,
+              width: "25rem",
+              borderColor: undefined,
+              zIndex: "2",
+            }),
+            menu: (provided) => ({ ...provided, zIndex: 9999 }),
+          }}
+          components={{
+            DropdownIndicator: () => null,
+            IndicatorSeparator: () => null,
+            SingleValue,
+          }}
+          onChange={(selected) =>
+            selected && updateUser((selected as OptionType).value)
+          }
+          defaultValue={{
+            label: user.email,
+            value: user,
+          }}
+          loadOptions={(inputValue: string) => {
+            if (inputValue.length >= 3) {
+              const d = client.ref("[GET]/user/search/:search").query({
+                params: { search: inputValue },
+              }) as Promise<QueryResult>;
+              return d.then((users) =>
+                users.map((u) => ({
+                  label: u.email,
+                  value: { ...u, role: u.role! },
+                }))
+              );
+            }
+          }}
+          loadingMessage={({ inputValue }) =>
+            inputValue.length >= 3
+              ? "Recherche..."
+              : "Veuillez rentrer au moins 3 lettres"
+          }
+          isClearable={true}
+          noOptionsMessage={({ inputValue }) =>
+            inputValue
+              ? "Pas d'établissement correspondant"
+              : "Commencez à écrire..."
+          }
+          placeholder="Email de l'utilisateur"
+          isMulti={false}
+        />
+      )}
+    </>
+  );
+};
+
+export { UserSearchBar };
---
+++ ui/app/(wrapped)/admin/roles/page.tsx
@@ -0,0 +1,145 @@
+"use client";
+
+import {
+  Box,
+  Heading,
+  HStack,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tooltip,
+  Tr,
+  VStack,
+} from "@chakra-ui/react";
+import _ from "lodash";
+import { useState } from "react";
+import { getPermissionScope, Permission, PERMISSIONS, Role } from "shared";
+import { Scope } from "shared/security/permissions";
+
+import { GuardPermission } from "@/utils/security/GuardPermission";
+
+import { themeDefinition } from "../../../../theme/theme";
+import { PermissionBadge } from "./components/PermissionBadge";
+import { User, UserSearchBar } from "./components/UserSearchBar";
+import { PERMISSION_GROUP_LABELS, ROLES_LABELS } from "./const";
+
+const ROLES = Object.keys(ROLES_LABELS) as Array<keyof typeof PERMISSIONS>;
+const ALL_PERMISSIONS: Array<Permission> = [];
+Object.keys(PERMISSIONS).forEach((key: string) => {
+  const role = key as Role;
+  const ROLE_PERMISSIONS = PERMISSIONS[role];
+  ALL_PERMISSIONS.push(...(Object.keys(ROLE_PERMISSIONS) as Permission[]));
+});
+
+const PERMISSION_GROUPS = _.uniq(ALL_PERMISSIONS.map((p) => p.split("/")[0]));
+
+const getPermissionsForRole = (role: Role) => {
+  return Object.keys(PERMISSIONS[role]) as Permission[];
+};
+
+const getPermissionsFromGroup = (
+  permissions: Array<Permission>,
+  group: string
+) => {
+  return permissions
+    .filter((p) => p.split("/")[0] === group)
+    .map((p) => p.split("/")[1]);
+};
+
+const getScopeFromGroup = (role: Role, group: string): Scope => {
+  const rolePermissions = PERMISSIONS[role];
+  const permission = Object.keys(rolePermissions).filter(
+    (key) => key.split("/")[0] === group
+  )[0] as Permission;
+
+  return getPermissionScope(role, permission)?.default ?? "national";
+};
+
+export default () => {
+  const [selectedUser, setSelectedUser] = useState<User>();
+
+  return (
+    <GuardPermission permission="users/ecriture">
+      <Box py="16px" px="64px" width="100%" display={"block"}>
+        <VStack gap="16px" width="100%">
+          <HStack gap="22px" width="100%" px="8px">
+            <Heading fontWeight={700} fontSize="17.5px">
+              Visualiser les permissions d’un utilisateur en fonction de son
+              rôle
+            </Heading>
+            <UserSearchBar updateUser={setSelectedUser} user={selectedUser} />
+          </HStack>
+          <TableContainer position="relative">
+            <Table variant="simple">
+              <Thead>
+                <Tr
+                  borderBottom={`1px solid ${themeDefinition.colors.grey[850]}`}
+                >
+                  <Th position="sticky">Rôle</Th>
+                  <Th
+                    borderRight={`1px solid ${themeDefinition.colors.grey[850]}`}
+                    position="sticky"
+                  >
+                    Description
+                  </Th>
+                  {PERMISSION_GROUPS.map((p) => (
+                    <Th key={p}>{PERMISSION_GROUP_LABELS[p]}</Th>
+                  ))}
+                </Tr>
+              </Thead>
+              <Tbody>
+                {ROLES.map((role) => (
+                  <Tr
+                    key={role}
+                    bgColor={
+                      role === selectedUser?.role
+                        ? themeDefinition.colors.bluefrance[950]
+                        : "transparent"
+                    }
+                    borderTopWidth="0px"
+                    borderBottomWidth="0px"
+                  >
+                    <Td
+                      position="sticky"
+                      textTransform={"uppercase"}
+                      fontSize="14px"
+                      fontWeight={700}
+                    >
+                      {ROLES_LABELS[role].label}
+                    </Td>
+                    <Td
+                      borderRight={`1px solid ${themeDefinition.colors.grey[850]}`}
+                      position="sticky"
+                      maxWidth="300px"
+                    >
+                      <Tooltip label={ROLES_LABELS[role].description}>
+                        <Text textOverflow={"ellipsis"} isTruncated>
+                          {ROLES_LABELS[role].description}
+                        </Text>
+                      </Tooltip>
+                    </Td>
+                    {PERMISSION_GROUPS.map((label) => (
+                      <Td key={label}>
+                        <PermissionBadge
+                          rights={getPermissionsFromGroup(
+                            getPermissionsForRole(role),
+                            label
+                          )}
+                          scope={getScopeFromGroup(role, label)}
+                        />
+                      </Td>
+                    ))}
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </VStack>
+      </Box>
+    </GuardPermission>
+  );
+};
