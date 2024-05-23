061a760b42145f0a46cc205fbb90a70d315a863d
Marin <marin.procureur@gmail.com>
2024-05-23 15:05:12 +0200

    fix (migrations): conflits de migration sur latestDemandeView

--- server/src/migrations/migration_1713431791519.ts
+++ server/src/migrations/migration_1713431791519.ts
@@ -2,7 +2,15 @@ import { Kysely, sql } from "kysely";
 
 export const up = async (db: Kysely<unknown>) => {
   // Drop view to enable altering the view column "statut" type
-  await db.schema.dropView("latestDemandeView").execute();
+  await db.schema
+    .dropView("latestDemandeView")
+    .materialized()
+    .ifExists()
+    .execute();
+  await db.schema
+    .dropView("latestDemandeNonMaterializedView")
+    .ifExists()
+    .execute();
 
   await db.executeQuery(
     sql`
@@ -11,38 +19,38 @@ export const up = async (db: Kysely<unknown>) => {
     `.compile(db)
   );
 
-  await db.schema
-    .createView("latestDemandeView")
-    .as(
-      // ts-ignore is mandatory here because we refresh views in this migration
-      // types are not yet infered from kysely codegen
-      // @ts-ignore
-      db
-        .selectFrom((sq) =>
-          // @ts-ignore
-          sq
-            .selectFrom("demande" as never)
-            // @ts-ignore
-            .select([
-              sql<number>`max("demande"."updatedAt")`.as("lastUpdatedAt"),
-              "numero",
-            ])
-            .distinct()
-            .groupBy("numero")
-            .as("latestDemandes")
-        )
-        // @ts-ignore
-        .leftJoin("demande", (join) =>
-          join
-            .onRef("latestDemandes.numero", "=", "demande.numero")
-            .onRef("latestDemandes.lastUpdatedAt", "=", "demande.updatedAt")
-        )
-        // @ts-ignore
-        .selectAll("demande")
-        // @ts-ignore
-        .where("demande.statut", "!=", "deleted")
-    )
-    .execute();
+  // await db.schema
+  //   .createView("latestDemandeView")
+  //   .as(
+  //     // ts-ignore is mandatory here because we refresh views in this migration
+  //     // types are not yet infered from kysely codegen
+  //     // @ts-ignore
+  //     db
+  //       .selectFrom((sq) =>
+  //         // @ts-ignore
+  //         sq
+  //           .selectFrom("demande" as never)
+  //           // @ts-ignore
+  //           .select([
+  //             sql<number>`max("demande"."updatedAt")`.as("lastUpdatedAt"),
+  //             "numero",
+  //           ])
+  //           .distinct()
+  //           .groupBy("numero")
+  //           .as("latestDemandes")
+  //       )
+  //       // @ts-ignore
+  //       .leftJoin("demande", (join) =>
+  //         join
+  //           .onRef("latestDemandes.numero", "=", "demande.numero")
+  //           .onRef("latestDemandes.lastUpdatedAt", "=", "demande.updatedAt")
+  //       )
+  //       // @ts-ignore
+  //       .selectAll("demande")
+  //       // @ts-ignore
+  //       .where("demande.statut", "!=", "deleted")
+  //   )
+  //   .execute();
 
   await db.schema
     .alterTable("demande")
