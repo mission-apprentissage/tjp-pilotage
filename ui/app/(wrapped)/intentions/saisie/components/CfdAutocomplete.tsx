749867b4aea575735d26d926446d090b102c954a
Marin <marin.procureur@gmail.com>
2024-06-04 15:30:27 +0200

    fix (lien-emploi-formation): lint

--- ui/app/(wrapped)/intentions/saisie/components/CfdAutocomplete.tsx
+++ ui/app/(wrapped)/intentions/saisie/components/CfdAutocomplete.tsx
@@ -60,7 +60,7 @@ export const CfdAutocompleteInput = ({
         if (search.length >= 3)
           return client
             .ref("[GET]/diplome/search/:search")
-            .query({ params: { search } });
+            .query({ params: { search }, query: {} });
       }}
       formatOptionLabel={(option) => {
         return (
