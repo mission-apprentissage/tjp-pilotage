c89f997dd6cc55d5791c00817bbe8e9694901aa7
Lucas Détré <lucas.detre@gmail.com>
2024-06-11 15:14:41 +0200

    feat: ajout de l'édito dans la page de synthèse perdir

--- ui/app/(wrapped)/intentions/perdir/synthese/[numero]/actions/EditoSection.tsx
+++ ui/app/(wrapped)/intentions/perdir/synthese/[numero]/actions/EditoSection.tsx
@@ -1,5 +1,58 @@
-import { chakra } from "@chakra-ui/react";
+"use client";
+
+import { LinkIcon } from "@chakra-ui/icons";
+import { Button, chakra, Divider, Flex, Heading } from "@chakra-ui/react";
+import NextLink from "next/link";
+
+import { client } from "@/api.client";
+import { STICKY_OFFSET } from "@/app/(wrapped)/intentions/perdir/SCROLL_OFFSETS";
+
+import { SyntheseSpinner } from "../components/SyntheseSpinner";
 
 export const EditoSection = chakra(() => {
-  return <></>;
+  const { data: editoContent, isLoading } = client
+    .ref("[GET]/edito")
+    .useQuery({}, { cacheTime: 0 });
+
+  if (isLoading) return <SyntheseSpinner />;
+  if (editoContent?.length === 0) return <></>;
+
+  return (
+    <Flex
+      direction={"column"}
+      gap={3}
+      bgColor={"white"}
+      borderRadius={6}
+      p={6}
+      top={STICKY_OFFSET}
+      position={"sticky"}
+    >
+      <Heading as="h2" fontSize={18} fontWeight={700} mb={4}>
+        Edito
+      </Heading>
+      {editoContent?.map((rowEdito, index) => {
+        return (
+          <Flex direction={"column"} gap={2} key={rowEdito.id}>
+            <Heading as="h3" fontSize={16} fontWeight={700}>
+              {rowEdito.titre}
+            </Heading>
+            <Heading as="h4" fontSize={14} fontWeight={400}>
+              {rowEdito.message}
+            </Heading>
+            {rowEdito.lien && (
+              <Button
+                as={NextLink}
+                href={rowEdito.lien}
+                variant={"externalLink"}
+                leftIcon={<LinkIcon />}
+              >
+                Lien
+              </Button>
+            )}
+            {editoContent.length - 1 !== index && <Divider />}
+          </Flex>
+        );
+      })}
+    </Flex>
+  );
 });
