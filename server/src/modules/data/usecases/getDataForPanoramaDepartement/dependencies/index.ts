cf1a22c40048d9b09ddf44226d19e1bd16096788
Marin <marin.procureur@gmail.com>
2024-07-18 11:32:21 +0200

    fix (harmonisation-transfo): Récupération des données régionales ij pour le panorama département

--- server/src/modules/data/usecases/getDataForPanoramaDepartement/dependencies/index.ts
+++ server/src/modules/data/usecases/getDataForPanoramaDepartement/dependencies/index.ts
@@ -1,3 +1,4 @@
 export { getFilters } from "./getFilters.dep";
 export { getFormationsDepartement } from "./getFormationsDepartement.dep";
 export { getTopFlopFormationsDepartement } from "./getTopFlopFormationsDepartement.dep";
+export { getCodeRegionFromDepartement } from "./getCodeRegionFromDepartement.dep";
