362a6e2741ee093f4bd479bba49c01fb601f31df
Lucas Détré <lucas.detre@gmail.com>
2024-08-09 10:16:45 +0200

    refactor: rm console.log

--- ui/app/(wrapped)/layout.tsx
+++ ui/app/(wrapped)/layout.tsx
@@ -13,7 +13,6 @@ export default async function RootLayout({
   readonly children: React.ReactNode;
 }) {
   const { isMaintenance } = await client.ref("[GET]/maintenance").query({});
-  console.log(isMaintenance);
 
   return (
     <>
