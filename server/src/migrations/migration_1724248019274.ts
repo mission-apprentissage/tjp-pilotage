9b5dcb1777a861a0ab2bb3e3f472de17425b2e9f
Gaetan BUSATO <gaetan.busato@gmail.com>
2024-08-21 15:59:33 +0200

    feat(tension): creation de la db tension et tensionRomeDepartement

---
+++ server/src/migrations/migration_1724248019274.ts
@@ -0,0 +1,30 @@
+import { Kysely } from "kysely";
+
+export const up = async (db: Kysely<unknown>) => {
+  await db.schema
+    .createTable("tension")
+    .addColumn("codeTension", "varchar(100)", (c) => c.notNull())
+    .addColumn("libelleTension", "varchar(100)", (c) => c.notNull())
+    .addUniqueConstraint("tension_unique_constraint", ["codeTension"])
+    .execute();
+
+  await db.schema
+    .createTable("tensionRomeDepartement")
+    .addColumn("codeRome", "varchar(5)", (c) =>
+      c.references("rome.codeRome").onDelete("cascade").notNull()
+    )
+    .addColumn("codeDepartement", "varchar(100)", (c) =>
+      c.references("departement.codeDepartement").onDelete("cascade").notNull()
+    )
+    .addColumn("codeTension", "varchar(100)", (c) =>
+      c.references("tension.codeTension").onDelete("cascade").notNull()
+    )
+    .addColumn("annee", "varchar(4)", (c) => c.notNull())
+    .addColumn("valeur", "integer", (c) => c.notNull())
+    .execute();
+};
+
+export const down = async (db: Kysely<unknown>) => {
+  await db.schema.dropTable("tensionRomeDepartement").execute();
+  await db.schema.dropTable("tension").execute();
+};
