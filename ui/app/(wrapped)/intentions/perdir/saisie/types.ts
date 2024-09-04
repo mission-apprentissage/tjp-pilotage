df2e9daf5e72718a9f4db5ac2b24f306bf9ba993
Gaetan BUSATO <gaetan.busato@gmail.com>
2024-09-04 16:19:40 +0200

    feat(avis): affichage des avis dans la boite de reception expe

--- ui/app/(wrapped)/intentions/perdir/saisie/types.ts
+++ ui/app/(wrapped)/intentions/perdir/saisie/types.ts
@@ -10,3 +10,6 @@ export type Order = Pick<Query, "order" | "orderBy">;
 export type Campagnes = (typeof client.infer)["[GET]/intentions"]["campagnes"];
 export type Campagne =
   (typeof client.infer)["[GET]/intention/:numero"]["campagne"];
+
+export type Avis =
+  (typeof client.infer)["[GET]/intentions"]["intentions"][number]["avis"][number];
