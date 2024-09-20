42116b85946f444a88bbf413e5df50976f9a11ee
Gaetan BUSATO <gaetan.busato@gmail.com>
2024-09-20 07:39:17 +0200

    fix(auth): mise à jour de la recherche des emails en lowercase

--- server/src/modules/core/usecases/sendResetPassword/sendResetPassword.schema.ts
+++ server/src/modules/core/usecases/sendResetPassword/sendResetPassword.schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const sendResetPasswordSchema = {
   body: z.object({
-    email: z.string().email(),
+    email: z.string().email().toLowerCase(),
   }),
   response: { 200: z.void() },
 };
