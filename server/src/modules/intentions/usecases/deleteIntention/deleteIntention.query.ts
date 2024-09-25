0182e2ca385b143a4512cab81361b761b7f03e94
Marin <marin.procureur@gmail.com>
2024-09-25 11:30:23 +0200

    fix (delete-intention): Ajout d'un userId pour l'updatedBy d'une intention lors de la suppression

--- server/src/modules/intentions/usecases/deleteIntention/deleteIntention.query.ts
+++ server/src/modules/intentions/usecases/deleteIntention/deleteIntention.query.ts
@@ -4,12 +4,17 @@ import { DemandeStatutEnum } from "shared/enum/demandeStatutEnum";
 import { Intention } from "../../../../db/schema";
 import { updateIntentionWithHistory } from "../../repositories/updateIntentionWithHistory.query";
 
-export const deleteIntentionQuery = async (
-  intention?: Insertable<Intention>
-) => {
+export const deleteIntentionQuery = async ({
+  intention,
+  updatedBy,
+}: {
+  intention?: Insertable<Intention>;
+  updatedBy: string;
+}) => {
   if (intention)
     updateIntentionWithHistory({
       ...intention,
       statut: DemandeStatutEnum["supprimée"],
+      updatedBy,
     });
 };
