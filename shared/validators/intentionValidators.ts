f3cd9a60991e53097e7bd7b64c54639c5425a7b3
Antoine Bigard <bigard.antoine@gmail.com>
2024-10-28 09:54:46 +0100

    refactor: putting back circular dependency 🤮

--- shared/validators/intentionValidators.ts
+++ shared/validators/intentionValidators.ts
@@ -1,10 +1,9 @@
 import type { Args, ZodTypeProvider } from "@http-wizard/core";
+import type { Router } from "server/src/server/routes/routes";
 
-// import type { Router } from "server/src/server/routes/routes";
 import { DemandeStatutEnum } from "../enum/demandeStatutEnum";
-// TODO TEMPARY ANY
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type Intention = Args<any["[POST]/intention/submit"]["schema"], ZodTypeProvider>["body"]["intention"];
+
+type Intention = Args<Router["[POST]/intention/submit"]["schema"], ZodTypeProvider>["body"]["intention"];
 
 export const isTypeFermeture = (typeDemande: string) => ["fermeture"].includes(typeDemande);
 
--- ui/api.client.ts
+++ ui/api.client.ts
@@ -1,16 +1,14 @@
 import type { ZodTypeProvider } from "@http-wizard/core";
 import { createQueryClient } from "@http-wizard/react-query";
 import axios from "axios";
+import type { Router } from "server/src/server/routes/routes";
 
-// import type { Router } from "server/src/server/routes";
 import { publicConfig } from "./config.public";
 
 // export const API_BASE_URL =
 //   typeof document === "undefined" ? process.env.NEXT_PUBLIC_APP_CONTAINER_URL : process.env.NEXT_PUBLIC_SERVER_URL;
 
-// TODO TEMPARY ANY
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const client = createQueryClient<any, ZodTypeProvider>({
+export const client = createQueryClient<Router, ZodTypeProvider>({
   instance: axios.create({
     baseURL: publicConfig.apiEndpoint,
     withCredentials: publicConfig.env === "local" ? false : true,
