19b3d5f70402349e68a04c1614c4ba207e4c1e72
Lucas Détré <lucas.detre@gmail.com>
2024-11-21 10:10:58 +0100

    fix: clean UI console

--- ui/app/(wrapped)/console/layout.tsx
+++ ui/app/(wrapped)/console/layout.tsx
@@ -4,7 +4,13 @@ import { Box } from "@chakra-ui/react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <Box p="0" display="flex" flexDirection={"column"} flex="1" minHeight="0">
+    <Box
+      p="0"
+      display="flex"
+      flexDirection={"column"}
+      flex="1"
+      minHeight="100%"
+    >
       {children}
     </Box>
   );
