36f857047e922f17e2210b778be6a15de368b70b
Marin <marin.procureur@gmail.com>
2024-11-25 17:48:28 +0100

    fix: cli actions context

--- server/src/basepath.ts
+++ server/src/basepath.ts
@@ -1,4 +1,6 @@
 import path from "path";
 
-export const basepath = path.join(__dirname, "../public");
-export const logspath = path.join(__dirname, "./modules/import/logs");
+import { __dirname } from "./utils/esmUtils";
+
+export const basepath = path.join(__dirname(), "../static");
+export const logspath = path.join(__dirname(), "./modules/import/logs");
