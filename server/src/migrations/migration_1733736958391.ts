cfdd25fdfefeb89e89eab9490cf17860d5a36af8
Lucas Détré <lucas.detre@gmail.com>
2024-12-16 17:19:09 +0100

    feat: mise en place de l'anonymisation automatique via pg_cron

--- server/src/migrations/migration_1733736958391.ts
+++ server/src/migrations/migration_1733736958391.ts
@@ -2,11 +2,13 @@ import type { Kysely } from "kysely";
 import { sql } from "kysely";
 
 export const up = async (db: Kysely<unknown>) => {
+  await sql`CREATE EXTENSION pg_cron;`.execute(db);
+
   await sql`
       CREATE OR REPLACE FUNCTION check_inactive_users() RETURNS trigger AS $$
       BEGIN
         IF COALESCE(OLD.lastSeenAt, OLD.createdAt) < NOW() - INTERVAL '2 years' THEN
-          NEW.email := CONCAT('anonyme_', NEW.id, '@orion.fr');
+          NEW.email := CONCAT('anonyme_', NEW.id, '@orion.inserjeunes.beta.gouv.fr');
           NEW.firstname := 'Utilisateur';
           NEW.lastname := 'Anonyme';
           NEW.password := NULL;
@@ -18,15 +20,19 @@ export const up = async (db: Kysely<unknown>) => {
   `.execute(db);
 
   await sql`
-    CREATE TRIGGER update_inactive_users_t
-    BEFORE UPDATE ON ${sql.table("user")}
-    FOR EACH ROW EXECUTE PROCEDURE check_inactive_users();
+    SELECT cron.schedule(
+      'anonymize_users_daily', -- Nom du job
+      '0 0 * * *',             -- Expression CRON (ici, chaque jour à minuit)
+      $$CALL check_inactive_users();$$ -- Appel de la fonction SQL
+    );
   `.execute(db);
+
+  await sql`SELECT * FROM cron.job;`.execute(db);
 
   await sql`
     UPDATE ${sql.table("user")}
     SET
-        email = CONCAT('anonyme_', id, '@orion.fr'),
+        email = CONCAT('anonyme_', id, '@orion.inserjeunes.beta.gouv.fr'),
         firstname = 'Utilisateur',
         lastname = 'Anonyme',
         password = NULL,
@@ -36,6 +42,9 @@ export const up = async (db: Kysely<unknown>) => {
 };
 
 export const down = async (db: Kysely<unknown>) => {
-  await sql`DROP TRIGGER update_inactive_users_t ON ${sql.table("user")}`.execute(db);
-  await sql`DROP FUNCTION check_inactive_users()`.execute(db);
+  await sql`SELECT cron.unschedule('anonymize_users_daily');`.execute(db);
+
+  await sql`DROP EXTENSION pg_cron;`.execute(db);
+
+  await sql`DROP FUNCTION check_inactive_users();`.execute(db);
 };
