22465b75f4f20b122461f43b51936b804e1652ea
Marin <marin.procureur@gmail.com>
2024-12-24 15:01:42 +0100

    chore: first RTL working test

---
+++ ui/components/__test__/AdvancedExportMenuButton.test.tsx
@@ -0,0 +1,33 @@
+import "@testing-library/jest-dom/vitest";
+
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { beforeAll, describe, expect, it, vi } from "vitest";
+
+import { AdvancedExportMenuButton } from "@/components/AdvancedExportMenuButton";
+
+const onExportCsv = vi.fn(async (f) => Promise.resolve());
+const onExportExcel = vi.fn(async (f) => Promise.resolve());
+
+describe("ui > components > AdvancedExportMenuButton", () => {
+  beforeAll(() => {
+    render(<AdvancedExportMenuButton onExportCsv={onExportCsv} onExportExcel={onExportExcel} />);
+  });
+
+  it("Doit afficher le bouton exporter", async () => {
+    const exporterButton = screen.queryByRole("button", { name: "Exporter" });
+
+    expect(exporterButton).not.toBeNull();
+  });
+
+  it("Doit afficher la modal après un click sur le bouton exporter", async () => {
+    const exporterButton = screen.queryByRole("button", { name: "Exporter" })!;
+    await userEvent.click(exporterButton);
+
+    // await chakra transition
+    await new Promise((r) => setTimeout(r, 200));
+
+    const menu = screen.queryByRole("menu");
+    expect(menu).not.toBeNull();
+  });
+});
