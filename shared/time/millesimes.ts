03337b4b84f8f108ed0def3096e4648564fc232b
Gaetan BUSATO <gaetan.busato@gmail.com>
2025-01-16 10:10:03 +0100

    chore(rs-2024): mise à jour des données de la rentrée scolaire 2024

--- shared/time/millesimes.ts
+++ shared/time/millesimes.ts
@@ -1,10 +1,10 @@
-export const MILLESIMES_IJ = ["2019_2020", "2020_2021", "2021_2022"];
+export const MILLESIMES_IJ = ["2019_2020", "2020_2021", "2021_2022", "2022_2023"];
 
-export const MILLESIMES_IJ_REG = ["2019_2020", "2020_2021", "2021_2022"];
+export const MILLESIMES_IJ_REG = ["2019_2020", "2020_2021", "2021_2022", "2022_2023"];
 
-export const MILLESIMES_IJ_ETAB = ["2020_2021", "2021_2022"];
+export const MILLESIMES_IJ_ETAB = ["2020_2021", "2021_2022", "2022_2023"];
 
-export const RENTREES_SCOLAIRES = ["2020", "2021", "2022", "2023"];
+export const RENTREES_SCOLAIRES = ["2020", "2021", "2022", "2023", "2024"];
 
 export const getMillesimeFromCampagne = (campagne: string) => {
   const rentreeScolaire = parseInt(campagne);
