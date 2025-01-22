2afa9bd5805b3a7f59289f78da6864314314de3e
Lucas Détré <lucas.detre@gmail.com>
2025-01-22 17:01:55 +0100

    fix: compteurs dans la boîte de réception des demandes/intentions

--- server/src/modules/demandes/usecases/countDemandes/countDemandes.usecase.ts
+++ server/src/modules/demandes/usecases/countDemandes/countDemandes.usecase.ts
@@ -11,10 +11,10 @@ const countDemandesFactory =
   ) =>
     async (activeFilters: Filters) => {
       const currentCampagne = await deps.getCurrentCampagneQuery();
-      const anneeCampagne = activeFilters.anneeCampagne ?? currentCampagne.annee;
+      const anneeCampagne = activeFilters.campagne ?? currentCampagne.annee;
 
       return await deps.countDemandesQuery({
-        anneeCampagne,
+        campagne: anneeCampagne,
         ...activeFilters,
       });
     };
