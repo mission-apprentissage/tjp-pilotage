52539e028b89661b9b7162f699768cc737a8cb5d
Gaetan BUSATO <gaetan.busato@gmail.com>
2025-01-23 15:41:09 +0100

    fix(intentions): mise à jour de la rs par défaut

--- ui/app/(wrapped)/intentions/perdir/saisie/new/page.tsx
+++ ui/app/(wrapped)/intentions/perdir/saisie/new/page.tsx
@@ -3,25 +3,30 @@
 import { CampagneStatutEnum } from "shared/enum/campagneStatutEnum";
 
 import { client } from "@/api.client";
+import { IntentionSpinner } from "@/app/(wrapped)/intentions/perdir/saisie/components/IntentionSpinner";
 import { IntentionForm } from "@/app/(wrapped)/intentions/perdir/saisie/intentionForm/IntentionForm";
 import { IntentionFilesProvider } from "@/app/(wrapped)/intentions/perdir/saisie/intentionForm/observationsSection/filesSection/filesContext";
 import { GuardPermission } from "@/utils/security/GuardPermission";
 
 // eslint-disable-next-line import/no-anonymous-default-export, react/display-name
 export default () => {
-  const { data: defaultCampagne } = client.ref("[GET]/campagne/expe/default").useQuery({});
+  const { data: defaultCampagne, isLoading } = client.ref("[GET]/campagne/expe/default").useQuery({});
 
   return (
     <GuardPermission permission="intentions-perdir/ecriture">
       <IntentionFilesProvider>
-        <IntentionForm
-          disabled={defaultCampagne?.statut !== CampagneStatutEnum["en cours"]}
-          defaultValues={{
-            campagneId: defaultCampagne?.id,
-          }}
-          formMetadata={{}}
-          campagne={defaultCampagne}
-        />
+        {isLoading && <IntentionSpinner />}
+        {!isLoading && defaultCampagne && (
+          <IntentionForm
+            disabled={defaultCampagne?.statut !== CampagneStatutEnum["en cours"]}
+            defaultValues={{
+              campagneId: defaultCampagne?.id,
+              rentreeScolaire: defaultCampagne?.annee ? Number.parseInt(defaultCampagne?.annee) + 1 : undefined,
+            }}
+            formMetadata={{}}
+            campagne={defaultCampagne}
+          />
+        )}
       </IntentionFilesProvider>
     </GuardPermission>
   );
