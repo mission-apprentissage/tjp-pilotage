ae35bdfa1be4432dad32ad07c4db1a4d75842536
Gaetan BUSATO <gaetan.busato@gmail.com>
2025-03-13 14:49:15 +0100

    fix: correction de la création et modification d'un utilisateur

---
+++ server/src/modules/core/usecases/editUser/dependencies/index.ts
@@ -0,0 +1,4 @@
+export { findUser } from "./findUser.dep";
+export { findDifferentUserWithSameEmail } from "./findUserWithEmail.dep";
+export { updateUser } from "./updateUser.dep";
+
--- server/src/modules/core/usecases/editUser/editUser.usecase.ts
+++ server/src/modules/core/usecases/editUser/editUser.usecase.ts
@@ -5,17 +5,31 @@ import type { BodySchema } from "shared/routes/schemas/put.users.userId.schema";
 
 import type { RequestUser } from "@/modules/core/model/User";
 
-import { updateUser } from "./updateUser.dep";
+import { findDifferentUserWithSameEmail, findUser, updateUser } from "./dependencies";
 import { canEditRole } from "./utils/canEditRole";
 import { canEditUserInScope } from "./utils/canEditUserInScope";
 
 export const [editUser, editUserFactory] = inject(
-  { updateUser },
+  { updateUser, findUser, findDifferentUserWithSameEmail },
   (deps) =>
     async ({ userId, data, requestUser }: { userId: string; data: BodySchema; requestUser: RequestUser }) => {
       if (!canEditRole({ requestUser, role: data.role })) throw Boom.unauthorized("cannot edit user with this role");
       if (!canEditUserInScope({ requestUser, body: data }))
         throw Boom.unauthorized("cannot edit user within this scope");
+
+      const user = await deps.findUser({ userId });
+
+      if(!user) {
+        throw Boom.notFound("Utilisateur inconnu dans l'application");
+      }
+
+      if(user.email !== data.email) {
+        const differentUserWithSameEmail = await deps.findDifferentUserWithSameEmail({ email: data.email, userId });
+        if(differentUserWithSameEmail) {
+          throw Boom.badRequest("Cette adresse email est déjà utilisée par un autre utilisateur");
+        }
+      }
+
       return await deps.updateUser({ userId, data });
     }
 );
