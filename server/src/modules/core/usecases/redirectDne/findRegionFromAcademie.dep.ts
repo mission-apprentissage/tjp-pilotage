ca89181dadd983e3b75a779f7e62a6ab368f7200
Marin <marin.procureur@gmail.com>
2025-04-23 14:46:58 +0200

    fix(sso): ajout de la gestion d'erreur lors de la recherche du codeRegion depuis codaca

--- server/src/modules/core/usecases/redirectDne/findRegionFromAcademie.dep.ts
+++ server/src/modules/core/usecases/redirectDne/findRegionFromAcademie.dep.ts
@@ -1,5 +1,8 @@
 import { getKbdClient } from "@/db/db";
-import { cleanNull } from "@/utils/noNull";
 
 export const findRegionFromAcademie = async (codeAcademie: string) =>
-  await getKbdClient().selectFrom("academie").select("codeRegion").where("codeAcademie", "=", codeAcademie).executeTakeFirst().then(cleanNull);
+  await getKbdClient()
+    .selectFrom("academie")
+    .select("codeRegion")
+    .where("codeAcademie", "=", codeAcademie)
+    .executeTakeFirst();
