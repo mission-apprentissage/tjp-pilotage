3717e48ffa922305ad3f584907b86e48e1febb44
Lucas Détré <lucas.detre@gmail.com>
2025-06-05 18:22:39 +0200

    feat: ajout d'un bouton plus visible pour les perdir pour les nouvelles demandes

--- ui/app/(wrapped)/components/Nav.tsx
+++ ui/app/(wrapped)/components/Nav.tsx
@@ -1,17 +1,7 @@
 "use client";
-import { ChevronDownIcon } from "@chakra-ui/icons";
-import {
-  Button,
-  chakra,
-  Flex,
-  Link,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
-  Portal,
-  useDisclosure,
-} from "@chakra-ui/react";
+import {ChevronDownIcon} from '@chakra-ui/icons';
+import {Box,Button, chakra, Flex, Link, Menu, MenuButton, MenuItem, MenuList, Portal, useDisclosure} from '@chakra-ui/react';
+import {Icon} from '@iconify/react';
 import NextLink from "next/link";
 import { useSelectedLayoutSegments } from "next/navigation";
 import { usePlausible } from "next-plausible";
@@ -378,6 +368,22 @@ export const Nav = () => {
           </Portal>
         </Menu>
       )}
+      {
+        hasRole({ user, role: RoleEnum["perdir"] }) && (
+          <Box display={"flex"} flexGrow={"1"} justifyContent={"end"} zIndex={"tooltip"} me={2}>
+            <Button
+              as={NextLink}
+              href="/demandes/saisie/new"
+              variant={"primary"}
+              fontSize={14}
+              color={"white"}
+              leftIcon={<Icon icon="ri:file-add-line" height={"20px"} />}
+            >
+                Nouvelle demande
+            </Button>
+          </Box>
+        )
+      }
       <Glossaire />
     </Flex>
   );
