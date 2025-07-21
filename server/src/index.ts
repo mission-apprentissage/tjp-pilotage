c84c768dd7f0c8dc1d8a14a81564a07f47eeff0f
Marin <marin.procureur@gmail.com>
2025-07-21 19:19:07 +0200

    fix(server): démarrage sans sentry

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,3 +2,6 @@ import { config } from "dotenv";
 
 config({ path: ".env" });
 config({ path: ".env.local", override: true });
+
+// Dynamic import to start server after env are loaded
+import("./main.js");
