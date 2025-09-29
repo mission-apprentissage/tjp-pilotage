be2117680c15e3db0304a51269a87cd2ece64a4f
Lucas Détré <lucas.detre@gmail.com>
2025-09-29 10:51:14 +0200

    fix: liens documentation

---
+++ ui/utils/getDocumentationPath.ts
@@ -0,0 +1,16 @@
+import { publicConfig } from "@/config.public";
+export const getDocumentationPath = (path: string) => {
+
+  switch(publicConfig.env) {
+  case "local":
+    return `http://localhost:8080/co/${path}`;
+  case "productionij":
+  case "production":
+  case "preproduction":
+  case "diffusion":
+  case "qualification":
+  default:
+    return `/documentation/${path}`;
+
+  }
+};
